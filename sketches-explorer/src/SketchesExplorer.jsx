59e179d49e1d984eb968150cdf49c2eea1257f11
Steeve Pommier <CostardRouge@users.noreply.github.com>
2021-12-24 03:33:45 +0100

    Updates

---
+++ sketches-explorer/src/SketchesExplorer.jsx
@@ -0,0 +1,7 @@
+import tree from "./tree.json";
+
+const SketchesExplorer = () => {
+  return <p>SketchesExplorer</p>;
+};
+
+export default SketchesExplorer;
---
+++ sketches-explorer/src/main.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+import SketchesExplorer from "./SketchesExplorer";
+
+ReactDOM.render(
+  <React.StrictMode>
+    <SketchesExplorer />
+  </React.StrictMode>,
+  document.getElementById("root")
+);
