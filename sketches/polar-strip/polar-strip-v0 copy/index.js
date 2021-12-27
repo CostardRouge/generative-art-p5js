282f1962f7927b858822d70d3ecd78eb1762aafc
Steeve Pommier <CostardRouge@users.noreply.github.com>
2021-12-28 00:43:15 +0100

    nft-gallery publish command

---
+++ sketches/polar-strip/polar-strip-v0 copy/index.js
@@ -0,0 +1,128 @@
+function setup() {
+  utils.canvas.create(utils.presets.FILL);
+  utils.events.fullScreenOnDoubleClick();
+  utils.events.extendCanvasOnResize();
+  utils.events.pauseOnSpaceKeyPressed();
+
+  const xCount = 1;
+  const yCount = 1;
+  const size = (width + height) / 2 / (xCount + yCount) / 4.5;
+
+  for (let x = 1; x <= xCount; x++) {
+    for (let y = 1; y <= yCount; y++) {
+      shapes.push(
+        new Spiral({
+          size,
+          relativePosition: {
+            x: x / (xCount + 1),
+            y: y / (yCount + 1),
+          },
+        })
+      );
+    }
+  }
+}
+
+function easeInOutQuint(x) {
+  return x < 0.5 ? 16 * x * x * x * x * x : 1 - pow(-2 * x + 2, 5) / 2;
+}
+
+function easeInOutBack(x) {
+  const c1 = 1.70158;
+  const c2 = c1 * 1.525;
+
+  return x < 0.5
+    ? (pow(2 * x, 2) * ((c2 + 1) * 2 * x - c2)) / 2
+    : (pow(2 * x - 2, 2) * ((c2 + 1) * (x * 2 - 2) + c2) + 2) / 2;
+}
+
+function easeOutElastic(x) {
+  const c4 = (2 * Math.PI) / 3;
+
+  return x === 0
+    ? 0
+    : x === 1
+    ? 1
+    : pow(2, -10 * x) * sin((x * 10 - 0.75) * c4) + 1;
+}
+
+class Spiral {
+  constructor(options) {
+    Object.assign(this, options);
+    this.calculateRelativePosition();
+  }
+
+  calculateRelativePosition() {
+    this.position = createVector(
+      lerp(0, width, this.relativePosition.x),
+      lerp(0, height, this.relativePosition.y)
+    );
+  }
+
+  onWindowResized() {
+    this.calculateRelativePosition();
+  }
+
+  draw(time, index, angleStep) {
+    let { position, size } = this;
+
+    const hueCadence = index + time;
+    push();
+    translate(position);
+
+    for (let angle = 0; angle < TAU; angle += angleStep) {
+      push();
+      translate(
+        utils.converters.polar.vector(angle, size)
+      );
+
+      const xOffset =
+        map(sin(angle + time), -1, 1, -PI, PI) *
+        easeInOutBack(map(sin(time + index), -1, 1, 0.2, 1));
+      const yOffset =
+        map(cos(angle + time), -1, 1, -PI, PI) *
+        easeInOutBack(map(sin(time + index), -1, 1, 0.2, 1));
+
+      const vector = createVector(
+        utils.converters.polar.get(sin, size, angle + xOffset),
+        utils.converters.polar.get(cos, size, angle + yOffset)
+      );
+      const nextVector = utils.converters.polar.vector(
+        cos,
+        angle + angleStep
+      );
+
+      beginShape();
+      strokeWeight(150);
+
+      stroke(
+        map(sin(angle + index + hueCadence), -1, 1, 0, 360),
+        map(cos(angle + index + hueCadence), -1, 1, 0, 255),
+        map(sin(angle + index + hueCadence), -1, 1, 255, 0)
+      );
+
+      vertex(vector.x, nextVector.y);
+      vertex(nextVector.x, vector.y);
+
+      endShape();
+      pop();
+    }
+
+    pop();
+  }
+}
+
+function draw() {
+  const seconds = frameCount / 60;
+  const time = seconds;
+
+  const angleAmountFactor = 192//utils.mappers.circularIndex(seconds, [2, 4, 8, 16, 32, 64, 128, 256]);
+  const angleAmount = angleAmountFactor / shapes.length;
+  const angleStep = TAU / angleAmount;
+
+  background(0);
+
+  shapes.forEach((shape, index) => shape.draw(time, index, angleStep));
+
+  utils.debug.fps();
+}
---
+++ sketches/worms/worms-v6-tentacule-aperturing/index.js
@@ -0,0 +1,135 @@
+function setup() {
+  utils.canvas.create(utils.presets.SQUARE.HD);
+  // utils.canvas.create(utils.presets.IPHONE_12.PORTRAIT);
+  // utils.canvas.create(utils.presets.FILL);
+
+  // utils.canvas.create({
+  //   width: 1080,
+  //   height: 1080,
+  //   ratio: 9 / 16,
+  // });
+
+  utils.events.extendCanvasOnResize();
+  utils.events.pauseOnSpaceKeyPressed();
+  utils.events.fullScreenOnDoubleClick();
+  utils.events.toggleCanvasRecordingOnKey();
+
+  const xCount = 1;
+  const yCount = 1;
+  const size = (width + height) / 2 / (xCount + yCount) / 2;
+
+  for (let x = 1; x <= xCount; x++) {
+    for (let y = 1; y <= yCount; y++) {
+      shapes.push(
+        new Spiral({
+          size,
+          weightRange: [200, 20],
+          opacityFactorRange: [5, 1],
+          relativePosition: {
+            x: x / (xCount + 1),
+            y: y / (yCount + 1),
+          },
+        })
+      );
+    }
+  }
+}
+
+class Spiral {
+  constructor(options) {
+    Object.assign(this, options);
+    this.calculateRelativePosition();
+  }
+
+  calculateRelativePosition() {
+    this.position = createVector(
+      lerp(0, width, this.relativePosition.x),
+      lerp(0, height, this.relativePosition.y)
+    );
+  }
+
+  onWindowResized() {
+    this.calculateRelativePosition();
+  }
+
+  draw(time, index) {
+    const { position, size, weightRange, opacityFactorRange } = this;
+    const hueCadence = index + time;
+    push();
+    translate(position.x, position.y);
+
+    const shadowsCount = 5;
+    const shadowIndexStep = 0.015;
+
+    for (
+      let shadowIndex = 0;
+      shadowIndex <= shadowsCount;
+      shadowIndex += shadowIndexStep
+    ) {
+      const weight = map(
+        shadowIndex,
+        0,
+        shadowsCount,
+        weightRange[0],
+        weightRange[1]
+      );
+
+      const opacityFactor = map(
+        shadowIndex,
+        0,
+        shadowsCount,
+        opacityFactorRange[0],
+        opacityFactorRange[1]
+      );
+
+      const l = map(sin(time + shadowIndex), -1, 1, 1.5, 0.2)/5;
+      const indexCoefficient = shadowIndex;
+      const x = map(sin(time * -2 + indexCoefficient), -1, 1, -l, l);
+      const y = map(cos(time + indexCoefficient), -1, 1, -l, l);
+
+      translate(x, y);
+
+      const i = map(sin(time+index), -1, 1, 25, 30);
+      const shadowOffset = radians(shadowIndex * i);
+      const angleStep = TAU / 8
+      for (let angle = 0; angle < TAU; angle += angleStep) {
+        push();
+        const vector = utils.converters.polar.vector(
+          angle + (index % 2 ? -time : time) * -1 + shadowOffset,
+          map(sin(time + shadowIndex), -1, 1, size * 0.1, size)
+        );
+
+        const vec = utils.converters.polar.vector(
+          angle + shadowOffset,
+          map(cos(time + shadowIndex), -1, 1, size * 0.1, size)
+        );
+
+        beginShape();
+        strokeWeight(weight);
+        stroke(
+          color(
+            map(sin(angle + hueCadence), -1, 1, 0, 360) / opacityFactor,
+            map(cos(angle + hueCadence), -1, 1, 360, 0) / opacityFactor,
+            map(sin(angle + hueCadence), -1, 1, 360, 0) / opacityFactor
+          )
+        );
+
+        vertex(vector.x + vec.x, vector.y + vec.y);
+        vertex(vector.x + vec.x, vector.y + vec.y);
+        // vertex(vector.x + vec.y, vector.y + vec.x);
+        // vertex(vector.x, vector.y);
+
+        endShape();
+        pop();
+      }
+    }
+
+    pop();
+  }
+}
+
+function draw() {
+  background(0);
+  shapes.forEach((shape, index) => shape.draw(utils.time.seconds(), index));
+  utils.debug.fps();
+}
