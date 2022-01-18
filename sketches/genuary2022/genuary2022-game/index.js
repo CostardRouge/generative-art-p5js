db6b989c07ebc36b58871de0dda4352b2fc6a6fa
Steeve Pommier <CostardRouge@users.noreply.github.com>
2022-01-18 09:00:50 +0100

    nft-gallery publish command

--- sketches/genuary2022/genuary2022-game/index.js
+++ sketches/genuary2022/genuary2022-game/index.js
@@ -1,13 +1,11 @@
 utils.sketch.setup(() => {
   const xCount = 5;
-  const yCount = 10;
-  const size = (width + height) / 2 / (xCount + yCount) / 30;
+  const yCount = 5;
 
   for (let x = 1; x <= xCount; x++) {
     for (let y = 1; y <= yCount; y++) {
       shapes.push(
         new Spiral({
-          size,
           weightRange: [150, 20],
           opacityFactorRange: [10, 1],
           relativePosition: {
@@ -18,7 +16,7 @@ utils.sketch.setup(() => {
       );
     }
   }
-},  { width: 768, height: 1368 });
+}, { width: 768, height: 1368 });
 
 class Spiral {
   constructor(options) {
@@ -38,17 +36,15 @@ class Spiral {
   }
 
   draw(time, index) {
-        const f = 1;
-
-    const { position, size, weightRange, opacityFactorRange } = this;
-    const hueCadence = index + time * f;
+    const { position, weightRange, opacityFactorRange } = this;
+    const hueCadence = index + time;
     push();
-    const x = map(sin(time + index), -1, 1, 0, position.x * 2);
-    const y = map(cos(time+index), -1, 1, 0, position.y * 2);
+    const x = map(sin(time + index/4), -1, 1, 0, position.x * 2);
+    const y = map(cos(time + index/4), -1, 1, 0, position.y * 2);
     translate(x % width, y % height);
 
     const shadowsCount = 15;
-    const shadowIndexStep = 0.1;
+    const shadowIndexStep = 0.3;
 
     for (
       let shadowIndex = 0;
@@ -68,50 +64,23 @@ class Spiral {
         0,
         shadowsCount,
         map(
-          sin(shadowIndex + time * f),
+          sin(shadowIndex + time),
           -1,
           1,
           opacityFactorRange[0],
           opacityFactorRange[0]/60
         ),
-        // opacityFactorRange[0],
-        opacityFactorRange[1]/2
+        opacityFactorRange[1]
       );
 
-      // const l = map(sin(time + shadowIndex), -1, 1, 0.5, 0.2)*2;
-      // const indexCoefficient = shadowIndex;
-      // const x = map(sin(time * -2 + indexCoefficient), -1, 1, -l, l);
-      // const y = map(cos(time * 2 + indexCoefficient), -1, 1, -l, l);
-
-      // translate(x, y);
-
-      // const i = map(sin(time/5 + index), -1, 1, 0, 100);
-      const shadowOffset = radians(shadowIndex * 100);
-      const angleStep = TAU / 1//map(sin(time/2), -1, 1, 10, 1);
-      for (let angle = 0; angle < TAU; angle += angleStep) {
+      for (let angle = 0; angle < TAU; angle += TAU / 1) {
         push();
-        // const vector = utils.converters.polar.vector(
-        //   angle + (index % 2 ? -time : time) * 0 + shadowOffset,
-        //   map(sin(time + shadowIndex), -1, 1, size * 0.2, size * 1.5)
-        // );
-
-        // const s = map(sin(time + shadowIndex), -1, 1, size * 0.2, size * 1.5);
-
-
-        const a = angle + (index % 2 ? -time : time) * -f + shadowOffset;
-        const w = 1//map(sin(time*f), -1, 1, 10, 1);
-        const h = 1//map(cos(time*-f), -1, 1, 5, 1);
-        const vector = createVector(
-          utils.converters.polar.get(sin, size*w, a),
-          utils.converters.polar.get(cos, size*h, a)
-        );
-
         beginShape();
         strokeWeight(weight);
         stroke(utils.colors.rainbow(hueCadence + angle, opacityFactor));
 
-        vertex(vector.x, vector.y);
-        vertex(vector.x, vector.y);
+        vertex(0, 0);
+        vertex(0, 0);
 
         endShape();
         pop();
