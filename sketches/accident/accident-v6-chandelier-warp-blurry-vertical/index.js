f72cd371bb8b192d7e15559f0a71e7dce62b308a
Steeve Pommier <CostardRouge@users.noreply.github.com>
2022-02-16 00:30:22 +0100

    blurry veritcal

---
+++ sketches/accident/accident-v6-chandelier-warp-blurry-vertical/index.js
@@ -0,0 +1,211 @@
+let target = null;
+let pixilatedCanvas = null;
+
+function setup() {
+    // utils.canvas.create(utils.presets.FILL);
+    // utils.canvas.create(utils.presets.SQUARE.RETINA);
+    // utils.canvas.create(utils.presets.SQUARE.HD);
+    utils.canvas.create({ width: 768, height: 1368 });
+    // utils.canvas.create({ width: 768/2, height: 1368/2 });
+    
+    utils.events.fullScreenOnDoubleClick();
+    utils.events.extendCanvasOnResize();
+    utils.events.pauseOnSpaceKeyPressed();
+    utils.events.toggleCanvasRecordingOnKey();
+    utils.events.toggleFPSCounter();
+    
+    noStroke();
+
+      pixilatedCanvas = createGraphics(
+        utils.canvas.main.width,
+        utils.canvas.main.height
+      );
+      pixilatedCanvas.pixelDensity(0.05);
+      pixilatedCanvas.noStroke();
+
+      utils.events.register("windowResized", () => {
+        pixilatedCanvas.width = utils.canvas.main.width;
+        pixilatedCanvas.height = utils.canvas.main.height;
+        pixilatedCanvas.pixelDensity(0.05);
+      });
+
+  const xCount = 1;
+  const yCount = 1;
+  const size = (width + height) / 2 / (xCount + yCount) / 3.5;
+
+  for (let x = 1; x <= xCount; x++) {
+    for (let y = 1; y <= yCount; y++) {
+      shapes.push(
+        new Spiral({
+          size,
+          start: createVector(0, -height / 3),
+          end: createVector(0, height / 3),
+          relativePosition: {
+            x: x / (xCount + 1),
+            y: y / (yCount + 1),
+          },
+        })
+      );
+    }
+  }
+
+  //  for (let x = 1; x <= xCount; x++) {
+  //    for (let y = 1; y <= yCount; y++) {
+  //      shapes.push(
+  //        new Spiral({
+  //          size,
+  //          start: createVector(-width / 3, 0),
+  //          end: createVector(width / 3, 0),
+  //          relativePosition: {
+  //            x: x / (xCount + 1),
+  //            y: y / (yCount + 1),
+  //          },
+  //        })
+  //      );
+  //    }
+  //  }
+}
+
+class Spiral {
+  constructor(options) {
+    Object.assign(this, options);
+    this.calculateRelativePosition();
+  }
+
+  calculateRelativePosition() {
+    this.position = createVector(
+      lerp(0, width, this.relativePosition.x),
+      lerp(0, height, this.relativePosition.y)
+    );
+  }
+
+  onWindowResized() {
+    this.calculateRelativePosition();
+  }
+
+  draw(time, index, target, lerpStep = 1 / 800) {
+    let { position, size, start, end } = this;
+
+    const hueCadence = index + time;
+    const waveAmplitude = size / 1.5//map(sin(time), -1, 1, 1, 2);
+
+    target.push();
+    target.translate(position.x, position.y);
+
+    for (let lerpIndex = 0; lerpIndex < 1; lerpIndex += lerpStep) {
+      //       const f = 15//map(lerpIndex, 0, 1, 1, 30)
+      //       const opacityFactor = map(
+      //         lerpIndex,
+      //         0,
+      //         1,
+      //         map(
+      //           sin(lerpIndex*f + time * 3),
+      //           -1,
+      //           1,
+      //           1,
+      //           2
+      //         ),
+      //         1
+      //       );
+
+      let angle = map(
+        lerpIndex,
+        0,
+        1.5,
+        map(sin(time / 2), -1, 1, -TAU, TAU),
+        -map(cos(time / 2), -1, 1, -TAU, TAU)
+      );
+      // angle = lerpIndex * 10;
+      // angle = map(
+      //   lerpIndex,
+      //   0,
+      //   1.5,
+      //   sin(lerpIndex) * TAU,
+      //   cos(lerpIndex) * TAU
+      // );
+      //  angle = map(lerpIndex, 0, 1/5, cos(TAU*lerpIndex), sin(TAU*lerpIndex));
+      angle = map(
+        lerpIndex,
+        0,
+        1.5,
+        sin(lerpIndex) * TAU,
+        cos(lerpIndex) * TAU
+      );
+      angle += map(
+        lerpIndex,
+        0,
+        1 / map(sin(time + index), -1, 1, -8, 8),
+        cos(lerpIndex * PI),
+        sin(lerpIndex * PI)
+      );
+      //  angle += map(
+      //    lerpIndex,
+      //    0,
+      //    1 / map(sin(time), -1, 1, -8, 8),
+      //    cos(lerpIndex * map(cos(time), -1, 1, -PI, PI)),
+      //    sin(lerpIndex * map(sin(time), -1, 1, -PI, PI))
+      //  );
+      angle += lerpIndex * 5 * (index + 1);
+      angle *= (index % 2 === 0 ? 1 : -1);
+      
+      const lerpPosition = p5.Vector.lerp(start, end, lerpIndex);
+      let waveIndex = angle * sin(-time + lerpIndex + index);
+      waveIndex = angle + time * 2;
+      const xOffset = map(sin(angle), -1, 1, -waveAmplitude, waveAmplitude);
+      const yOffset = map(cos(angle), 1, -1, -waveAmplitude, waveAmplitude);
+
+      target.fill(
+        map(sin(angle + hueCadence), -1, 1, 0, 360) / 1,
+        map(cos(angle + hueCadence), -1, 1, 0, 255) / 1,
+        map(sin(angle + hueCadence), -1, 1, 255, 0) / 1
+      );
+
+      let s = 25;
+      s = map(sin(time + waveIndex + lerpIndex), -1, 1, 10, 70);
+
+      // target.translate(map(sin(waveIndex + index), -1, 1, -1, 1), 0);
+
+      let c = 3 + index; // map(sin(time+waveIndex), -1, 1, 2, 5);
+      c = map(sin(time + lerpIndex * waveIndex), -1, 1, 1, 4);
+
+      for (let i = 0; i < c; i++) {
+        const x = lerp(
+          lerpPosition.x + xOffset,
+          lerpPosition.x - xOffset,
+          i / c
+        );
+        const y = lerp(
+          lerpPosition.y + yOffset,
+          lerpPosition.y - yOffset,
+          i / c
+        );
+
+        target.circle(x, y, s);
+      }
+
+      // target.circle(lerpPosition.x + xOffset, lerpPosition.y + yOffset, s);
+      // target.circle(lerpPosition.x - xOffset, lerpPosition.y - yOffset, s);
+    }
+
+    target.pop();
+  }
+}
+
+utils.sketch.draw((time) => {
+  noSmooth()
+
+  background(0);
+
+  pixilatedCanvas.filter(BLUR, 1);
+  pixilatedCanvas.background(0, 0, 0, 2);
+  image(pixilatedCanvas, 0, 0);
+
+
+  // pixilatedCanvas.pixelDensity(map(sin(time), -1, 1, 0.01, 0.05));
+
+
+  shapes.forEach((shape, index) => {
+    shape.draw(-time, index, pixilatedCanvas, 1 / 300);
+    // shape.draw(-time, index, window, 1 / 800);
+  });
+});
