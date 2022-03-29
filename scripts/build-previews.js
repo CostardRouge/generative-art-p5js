feebc11d53b482a085ca8858b1b57c6103876474
Steeve Pommier <CostardRouge@users.noreply.github.com>
2022-03-30 00:04:22 +0200

    v1

--- scripts/build-previews.js
+++ scripts/build-previews.js
@@ -10,10 +10,6 @@ let _page;
 async function getScreenshot(html) {
   const [ width, height ] = size.split( 'x' ).map( Number );
 
-  if (_page) {
-    return _page;
-  }
-
   const browser = await core.launch({
     args: [],
     executablePath: '/Applications/Google Chrome.app/Contents/MacOS/Google Chrome',
@@ -26,20 +22,48 @@ async function getScreenshot(html) {
   return await page.screenshot({ type });
 }
 
-getScreenshot("<html><body><h1>Hello World</h1></body></html>")
-  .then(function(screenShootFileContent) {
-    writeFileSync(`./file.${type}`, screenShootFileContent)
+async function getPage() {
+  if (_page) {
+    return _page;
+  }
+
+  const browser = await core.launch({
+    args: [],
+    executablePath: '/Applications/Google Chrome.app/Contents/MacOS/Google Chrome',
+    headless: true
   });
 
-  const treeFileContent = readFileSync(treePath, 'utf8');
+  _page = await browser.newPage();
+
+  return _page;
+}
+
+async function getScreenshotWithURL(url) {
+  const page = await getPage();
+  const [ width, height ] = size.split( 'x' ).map( Number );
+
+  await page.setViewport({ width, height});
+  await page.goto(url, { waitUntil: 'domcontentloaded' });
+  await page.waitForSelector('.p5Canvas');
+
+  return await page.screenshot({ type });
+}
+
+const treeFileContent = readFileSync(treePath, 'utf8');
 const tree = JSON.parse(treeFileContent);
 
 for (const [ , sketches ] of Object.entries(tree)) {
   for (const [ sketchName, sketch ] of Object.entries(sketches)) {
-    const indexPath = `${sketch.path}/index.html`;
-    const indexFileContent = readFileSync(indexPath, 'utf8');
 
-    console.log(sketchName, indexFileContent);
+    //const indexPath = `${sketch.path}/index.html`;
+    //const indexFileContent = readFileSync(indexPath, 'utf8');
+    const screenShootFilePath = `${sketch.path}/screenshoot.${type}`;
+    const indexFileURL = `http://localhost:5500/${sketch.path}/?preview&size=${size}`;
+    const screenShootFileContent = await getScreenshotWithURL(indexFileURL);
+
+    writeFileSync(screenShootFilePath, screenShootFileContent);
+
+    console.log("OK: ", sketchName, screenShootFilePath);
   }
 }
 
