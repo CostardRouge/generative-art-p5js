c6d4293e997beeed4ab064e85ad71db00ed47a21
Steeve Pommier <CostardRouge@users.noreply.github.com>
2022-03-31 21:45:18 +0200

    sketch-gallery publish command

---
+++ sketches/pulse/pulse-v7-turbine-eccentric/index.js
@@ -0,0 +1,191 @@
+import { shapes, sketch, converters, canvas, events, colors, mappers, options } from './utils/index.js';
+
+options.add( [
+  // {
+  //   id: "shapes-count",
+  //   type: 'number',
+  //   label: 'Shapes count',
+  //   min: 1,
+  //   max: 1000,
+  //   defaultValue: 500,
+  //   category: 'Integers'
+  // },
+  {
+    id: "lines-count",
+    type: 'number',
+    label: 'Max lines count',
+    min: 1,
+    max: 32,
+    defaultValue: 3,
+    category: 'Integers'
+  },
+  {
+    id: "start-size",
+    type: 'number',
+    label: 'Start size',
+    min: 1,
+    max: 300,
+    step: 10,
+    defaultValue: 50,
+    category: 'Integers'
+  },
+  {
+    id: "end-size",
+    type: 'number',
+    label: 'End size',
+    min: 1,
+    max: 300,
+    step: 10,
+    defaultValue: 15,
+    category: 'Integers'
+  },
+  {
+    id: "start-opacity-factor",
+    type: 'slider',
+    label: 'Start opacity (reduction factor)',
+    min: 1,
+    max: 50,
+    defaultValue: 3,
+    category: 'Integers'
+  },
+  {
+    id: "end-opacity-factor",
+    type: 'slider',
+    label: 'End opacity (reduction factor)',
+    min: 1,
+    max: 50,
+    defaultValue: 1,
+    category: 'Integers'
+  },
+  {
+    id: "size-ratio",
+    type: 'number',
+    label: 'Size ratio',
+    min: 1,
+    max: 10,
+    defaultValue: 6,
+    category: 'Integers'
+  }
+] );
+
+sketch.setup(() => {
+  const xCount = 1;
+  const yCount = 1;
+
+  for (let x = 1; x <= xCount; x++) {
+    for (let y = 1; y <= yCount; y++) {
+      shapes.push(
+        new Spiral({
+          relativePosition: {
+            x: x / (xCount + 1),
+            y: y / (yCount + 1),
+          },
+        })
+      );
+    }
+  }
+});
+
+class Spiral {
+  constructor(options) {
+    Object.assign(this, options);
+    this.calculateRelativePosition();
+  }
+
+  calculateRelativePosition() {
+    this.position = createVector(
+      lerp(0, width, this.relativePosition.x),
+      lerp(0, height, this.relativePosition.y)
+    );
+  }
+
+  onWindowResized() {
+    this.calculateRelativePosition();
+  }
+
+  draw(time, index) {
+    const { position } = this;
+    push();
+    translate(position.x, position.y);
+
+    const shadowsCount = map(cos(index+time*3)+sin(-time/3+index), -1, 1, 1, 5, true)
+    const shadowIndexStep = 0.01; //map(sin(time), -1, 1, 0.2, 0.05);
+
+    for (
+      let shadowIndex = 0;
+      shadowIndex <= shadowsCount;
+      shadowIndex += shadowIndexStep
+    ) {
+      const size = map(
+        shadowIndex,
+        0,
+        shadowsCount,
+        options.get("start-size"),
+        options.get("end-size")
+      );
+
+      const opacityFactor = map(
+        shadowIndex,
+        0,
+        shadowsCount,
+        map(
+          sin(index -time * 5 + shadowIndex * 5),
+          -1,
+          1,
+          options.get("start-opacity-factor"),
+          options.get("start-opacity-factor") * 5
+        ),
+        options.get("end-opacity-factor")
+      );
+
+      const l = shadowIndex/3;
+      const indexCoefficient = shadowIndex + index;
+      const x = map(sin(time + sin(time*2) - indexCoefficient), -1, 1, -l, l);
+      const y = map(cos(time * 2 - indexCoefficient), -1, 1, -l, l);
+
+      translate(x*1.5, y*3);
+
+      // const angleStep = TAU / options.get("lines-count")*((index+1)*2)
+      const angleStep = TAU / map(sin(time+index)+cos(-time+index), -1, 1, 1, 7, true)
+
+      for (let angle = 0; angle < TAU; angle += angleStep) {
+        const vector = converters.polar.vector(
+          angle,// * sin(time),
+          size * options.get("size-ratio")
+        );
+        push();
+
+        beginShape();
+        strokeWeight(size);
+
+        rotate(sin(time+0+shadowIndex)*1 + 0);
+
+        stroke(
+          color(
+            map(sin(-time+shadowIndex+index+(shadowIndex/5)*10), -1, 1, 0, 360) /
+              opacityFactor,
+            map(cos(-time-shadowIndex+index-(shadowIndex/2.5)*5), -1, 1, 360, 0) /
+              opacityFactor,
+            map(sin(-time+shadowIndex+index+(shadowIndex/1)*1), -1, 1, 360, 0) /
+              opacityFactor,
+              // 50
+          )
+        );
+        
+        vertex(vector.x, vector.y);
+        vertex(-vector.x, -vector.y);
+
+        endShape();
+        pop();
+      }
+    }
+
+    pop();
+  }
+}
+
+sketch.draw((time) => {
+  background(0);
+
+  shapes.forEach((shape, index) => shape.draw(time, index));
+});
---
+++ sketches/pulse/pulse-v7-turbine-flower/utils/sketch.js
@@ -0,0 +1,48 @@
+import { canvas, events, time, debug, options } from './index.js';
+
+const sketch = {
+  name: location.pathname.split("/").slice(1, -1).join("-"),
+  setup: (
+    setup,
+    canvasOptions
+  ) => {
+    sketch.setup = () => {
+      options.init()
+      const canvasSize = (
+        (new URLSearchParams(document.location.search)).get('size')     ??
+        options.get("canvas-size")                                      ??
+        `${canvas.configuration.width}x${canvas.configuration.height}`
+      );
+      const [width, height] = canvasSize.split('x').map(Number);
+
+      canvas.create({
+        width: canvasSize === 'fill' ? windowWidth : width,
+        height: canvasSize === 'fill' ? windowHeight : height,
+        ...canvasOptions
+      });
+      
+      events.toggleNoLoopOnSingleClick();
+      events.toggleCanvasRecordingOnKey();
+      events.pauseOnSpaceKeyPressed();
+      events.toggleFPSCounterOnKeyPressed();
+      events.extendCanvasOnFullScreen();
+      events.extendCanvasOnResize();
+      events.toggleFullScreenOnDoubleClick();
+
+      noStroke();
+
+      setup?.();
+    };
+  },
+  draw: (draw) => {
+    sketch.draw = () => {
+      debug.fps();
+      pixelDensity(options.get("pixel-density"));
+      frameRate(options.get("framerate"));
+      noSmooth();
+      draw?.(time.seconds() * options.get("time-speed"));
+    };
+  },
+};
+
+export default sketch;
