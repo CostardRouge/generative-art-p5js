873fb891847e44a139ed3fa76113609075baf833
Steeve Pommier <CostardRouge@users.noreply.github.com>
2022-04-03 17:38:22 +0200

    MOBILE FRIENDLY

--- utils/options.js
+++ utils/options.js
@@ -2,122 +2,140 @@ import SketchUI from '../libraries/sketch-ui.es.js';
 
 import { canvas, recorder, debug, sketch } from './index.js';
 
-const getDefaultOptions = () => [
-  {
-    id: 'pause-canvas-on-single-click',
-    type: 'switch',
-    label: 'Pause canvas on single click',
-    defaultValue: false,
-    category: 'Events'
-  },
-  {
-    id: 'press-r-to-record',
-    type: 'switch',
-    label: 'Press R to record',
-    defaultValue: true,
-    category: 'Events'
-  },
-  {
-    id: 'pause-on-space-key-pressed',
-    type: 'switch',
-    label: 'Pause canvas on space key',
-    defaultValue: true,
-    category: 'Events'
-  },
-  {
-    id: 'press-f-to-toggle-fps-counter',
-    type: 'switch',
-    label: 'Press F to toggle FPS counter',
-    defaultValue: true,
-    category: 'Events'
-  },
-  {
-    id: 'extend-canvas-on-full-screen',
-    type: 'switch',
-    label: 'Extend canvas on full screen',
-    defaultValue: false,
-    category: 'Events'
-  },
-  {
-    id: 'extend-canvas-on-resize',
-    type: 'switch',
-    label: 'Extend canvas on resize',
-    defaultValue: false,
-    category: 'Events'
-  },
-  {
-    id: 'toggle-full-screen-on-double-click',
-    type: 'switch',
-    label: 'Toggle full screen on double click',
-    defaultValue: true,
-    category: 'Events'
-  },
-  {
-    id: 'show-fps',
-    type: 'switch',
-    label: 'Show framerate',
-    defaultValue: debug.options.fps.display,
-    category: 'Debug'
-  },
-  {
-    id: 'pixel-density',
-    type: 'number',
-    label: 'Pixel density',
-    min: 0.1,
-    max: 4,
-    precision: 1,
-    step: 0.1,
-    defaultValue: pixelDensity(),
-    category: 'Debug'
-  },
-  {
-    id: 'smooth-pixel',
-    type: 'switch',
-    label: 'Smooth pixel',
-    defaultValue: true,
-    category: 'Debug'
-  },
-  {
-    id: 'framerate',
-    type: 'slider',
-    label: 'Framerate',
-    defaultValue: 60,
-    min: 1,
-    max: 120,
-    marks: [
-      { value: 20, label: '20 fps' },
-      { value: 60, label: '60 fps' },
-      { value: 100, label: '100 fps' },
-    ],
-    category: 'Debug'
-  },
-  {
-    id: 'time-speed',
-    type: 'slider',
-    label: 'General time speed',
-    defaultValue: 1,
-    step: 0.1,
-    min: -3,
-    max: 3,
-    marks: [
-      { value: -3, label: '-3' },
-      { value: 0, label: 'stop' },
-      { value: 3, label: '3' },
-    ],
-    category: 'Debug'
-  },
-  {
-    type: 'button',
-    text: 'Toggle fullscreen',
-    icon: 'ScreenShare',
-    onClick: () => canvas.fullscreen(),
-    category: 'Canvas'
-  },
-  {
+const getDefaultOptions = () => {
+  // const { type } = screen.orientation;
+  // const isMobile = type !== "landscape-primary" && type !== "landscape-secondary";
+
+  const isMobile = /iPhone|iPad|iPod|Android/i.test(navigator.userAgent);
+
+  if (isMobile) {
+    document.querySelector('body').style.overflow = 'hidden';
+  }
+
+  const defaultOptions = [];
+  const defaultPixelDensity = isMobile ? 1 : pixelDensity();
+  const defaultFramerate = 60;
+  const defaultTimeSpeed = 1;
+  const defaultCanvasSize = isMobile ? 'fill' :`${canvas.configuration.width}x${canvas.configuration.height}`;
+
+  defaultOptions.push( ...[
+    {
+      id: 'pause-canvas-on-single-click',
+      type: 'switch',
+      label: 'Pause canvas on single click',
+      defaultValue: false,
+      category: 'Events'
+    },
+    {
+      id: 'press-r-to-record',
+      type: 'switch',
+      label: 'Press R to record',
+      defaultValue: true,
+      category: 'Events'
+    },
+    {
+      id: 'pause-on-space-key-pressed',
+      type: 'switch',
+      label: 'Pause canvas on space key',
+      defaultValue: true,
+      category: 'Events'
+    },
+    {
+      id: 'press-f-to-toggle-fps-counter',
+      type: 'switch',
+      label: 'Press F to toggle FPS counter',
+      defaultValue: true,
+      category: 'Events'
+    },
+    {
+      id: 'extend-canvas-on-full-screen',
+      type: 'switch',
+      label: 'Extend canvas on full screen',
+      defaultValue: false,
+      category: 'Events'
+    },
+    {
+      id: 'extend-canvas-on-resize',
+      type: 'switch',
+      label: 'Extend canvas on resize',
+      defaultValue: false,
+      category: 'Events'
+    },
+    {
+      id: 'toggle-full-screen-on-double-click',
+      type: 'switch',
+      label: 'Toggle full screen on double click',
+      defaultValue: true,
+      category: 'Events'
+    },
+    {
+      id: 'show-fps',
+      type: 'switch',
+      label: 'Show framerate',
+      defaultValue: debug.options.fps.display,
+      category: 'Debug'
+    },
+    {
+      id: 'pixel-density',
+      type: 'number',
+      label: 'Pixel density',
+      min: 0.1,
+      max: 4,
+      precision: 1,
+      step: 0.1,
+      defaultValue: defaultPixelDensity,
+      category: 'Debug'
+    },
+    {
+      id: 'smooth-pixel',
+      type: 'switch',
+      label: 'Smooth pixel',
+      defaultValue: true,
+      category: 'Debug'
+    },
+    {
+      id: 'framerate',
+      type: 'slider',
+      label: 'Framerate',
+      defaultValue: defaultFramerate,
+      min: 1,
+      max: 120,
+      marks: [
+        { value: 20, label: '20 fps' },
+        { value: 60, label: '60 fps' },
+        { value: 100, label: '100 fps' },
+      ],
+      category: 'Debug'
+    },
+    {
+      id: 'time-speed',
+      type: 'slider',
+      label: 'General time speed',
+      defaultValue: defaultTimeSpeed,
+      step: 0.1,
+      min: -3,
+      max: 3,
+      marks: [
+        { value: -3, label: '-3' },
+        { value: 0, label: 'stop' },
+        { value: 3, label: '3' },
+      ],
+      category: 'Debug'
+    },
+    {
+      type: 'button',
+      text: 'Toggle fullscreen',
+      icon: 'ScreenShare',
+      onClick: () => canvas.fullscreen(),
+      category: 'Canvas'
+    }
+  ] );
+
+  defaultOptions.push( {
     id: "canvas-size",
     type: 'select',
     label: 'Canvas size',
-    defaultValue: `${canvas.configuration.width}x${canvas.configuration.height}`,
+    defaultValue: defaultCanvasSize,
     onChange: value => {
       let [width, height] = value.split('x').map(Number);
       
@@ -151,7 +169,7 @@ const getDefaultOptions = () => [
       },
       {
         label: `Default ${canvas.configuration.width} x ${canvas.configuration.height}`,
-        value: `${canvas.configuration.width}x${canvas.configuration.height}`,
+        value: defaultCanvasSize,
         group: 'Portrait'
       },
       {
@@ -186,82 +204,88 @@ const getDefaultOptions = () => [
       }
     ],
     category: 'Canvas'
-  },
-  {
-    type: 'button',
-    text: 'Clear',
-    icon: 'Eraser',
-    onClick: () => clear(),
-    category: 'Canvas'
-  },
-  {
-    type: 'button',
-    text: 'Pause',
-    icon: 'PlayerPause',
-    onClick: () => noLoop(),
-    category: 'Canvas'
-  },
-  {
-    type: 'button',
-    text: 'Draw next frame',
-    icon: 'PlayerTrackNext',
-    onClick: () => redraw(),
-    category: 'Canvas'
-  },
-  {
-    type: 'button',
-    text: 'Resume',
-    icon: 'PlayerPlay',
-    onClick: () => loop(),
-    category: 'Canvas'
-  },
-  {
-    id: 'recording-format',
-    type: 'select',
-    label: 'Video recording format',
-    defaultValue: 'png',
-    options: [
-      {
-        value: 'png',
-        label: '.png',
-      },
-      {
-        value: 'jpg',
-        label: '.jpg',
-      },
-      {
-        value: 'gif',
-        label: '.gif',
-      },
-      {
-        value: 'webm',
-        label: '.webm',
-      }
-    ],
-    category: 'Canvas'
-  },
-  {
-    type: 'button',
-    text: 'Start recording',
-    icon: 'Circle',
-    onClick: () => recorder.start(),
-    category: 'Canvas'
-  },
-  {
-    type: 'button',
-    text: 'Stop recording',
-    icon: 'PlayerStop',
-    onClick: () => recorder.stop(),
-    category: 'Canvas'
-  },
-  {
-    type: 'button',
-    text: 'Save .png',
-    icon: 'DeviceFloppy',
-    onClick: () => canvas.save(),
-    category: 'Canvas'
-  }
-];
+  } );
+
+    
+  defaultOptions.push( ...[
+    {
+      type: 'button',
+      text: 'Clear',
+      icon: 'Eraser',
+      onClick: () => clear(),
+      category: 'Canvas'
+    },
+    {
+      type: 'button',
+      text: 'Pause',
+      icon: 'PlayerPause',
+      onClick: () => noLoop(),
+      category: 'Canvas'
+    },
+    {
+      type: 'button',
+      text: 'Draw next frame',
+      icon: 'PlayerTrackNext',
+      onClick: () => redraw(),
+      category: 'Canvas'
+    },
+    {
+      type: 'button',
+      text: 'Resume',
+      icon: 'PlayerPlay',
+      onClick: () => loop(),
+      category: 'Canvas'
+    },
+    {
+      id: 'recording-format',
+      type: 'select',
+      label: 'Video recording format',
+      defaultValue: 'png',
+      options: [
+        {
+          value: 'png',
+          label: '.png',
+        },
+        {
+          value: 'jpg',
+          label: '.jpg',
+        },
+        {
+          value: 'gif',
+          label: '.gif',
+        },
+        {
+          value: 'webm',
+          label: '.webm',
+        }
+      ],
+      category: 'Canvas'
+    },
+    {
+      type: 'button',
+      text: 'Start recording',
+      icon: 'Circle',
+      onClick: () => recorder.start(),
+      category: 'Canvas'
+    },
+    {
+      type: 'button',
+      text: 'Stop recording',
+      icon: 'PlayerStop',
+      onClick: () => recorder.stop(),
+      category: 'Canvas'
+    },
+    {
+      type: 'button',
+      text: 'Save .png',
+      icon: 'DeviceFloppy',
+      onClick: () => canvas.save(),
+      category: 'Canvas'
+    }
+  ] );
+
+  return defaultOptions;
+};
 
 const options = {
   sketchUI: undefined,
