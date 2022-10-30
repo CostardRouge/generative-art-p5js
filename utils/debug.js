895469bb98f6fac0613bee62df368f77dad71237
CostardRouge <CostardRouge@users.noreply.github.com>
2022-10-30 09:57:21 +0000

    Deploying to gh-pages from @ CostardRouge/generative-art-p5js@5778ab3ed4b7e80314056ac7b82970ecc8669ed6 🚀

--- utils/debug.js
+++ utils/debug.js
@@ -14,7 +14,6 @@ const debug = {
   },
   //debugGraphics: undefined,
   DOMElements: {
-    fpsCounter: undefined,
     canvasOverlay: undefined
   },
   // createDebugGraphics: () => {
@@ -23,6 +22,27 @@ const debug = {
   toggleFPSCounter: () => {
     debug.options.fps.display = !debug.options.fps.display;
     options.set( 'show-fps', debug.options.fps.display );
+  },
+  createElement: ( targetQuerySelector, tagName, contentCreator, remove ) => {
+    if (true === remove) {
+      if (undefined === debug.DOMElements[tagName]) {
+        return;
+      }
+
+      debug.DOMElements[tagName].remove();
+      debug.DOMElements[tagName] = undefined;
+      return;
+    }
+
+    if (debug.DOMElements[tagName] === undefined) {
+      debug.DOMElements[tagName] = document.createElement( tagName );
+
+      document
+        .querySelector(targetQuerySelector)
+        .appendChild( debug.DOMElements[tagName] )
+    }
+
+    debug.DOMElements[tagName].innerHTML = contentCreator();
   },
   fps: () => {
     debug.options.fps.display = options.get( 'show-fps');
@@ -39,35 +59,7 @@ const debug = {
       }
     });
 
-    if (display === true) {
-      if (debug.DOMElements.fpsCounter === undefined) {
-        //debug.createDebugGraphics();
-        debug.DOMElements.fpsCounter = createElement(
-          "fps-counter",
-          String(ceil(debug.lastFrameRate))
-        );
-      }
-
-      debug.DOMElements.fpsCounter.elt.innerHTML = String(ceil(debug.lastFrameRate));
-
-      // debug.debugGraphics.clear();
-
-      // string.write(
-      //   String(ceil(debug.lastFrameRate)),
-      //   18,
-      //   16,
-      //   18,
-      //   text.defaultFont,
-      //   debug.debugGraphics
-      // );
-
-      // image(debug.debugGraphics, 0, 0);
-    } else {
-      if (debug.DOMElements.fpsCounter !== undefined) {
-        debug.DOMElements.fpsCounter.elt.remove();
-        debug.DOMElements.fpsCounter = undefined;
-      }
-    }
+    debug.createElement( "body", "fps-counter", () => ceil(debug.lastFrameRate), !display)
   },
   print: function (what, printOptions = {}) {
     const { frequency } = { ...debug.options.print, ...printOptions };
@@ -86,35 +78,26 @@ const debug = {
     line(0, mouseY, width, mouseY);
   },
   webgl: function() {
-    if (!sketch.camera) {
-      return;
+    const createContent = () => {
+      const camera = sketch.camera;
+
+      return `camera
+        eyeX: ${camera.eyeX}<br>
+        eyeY: ${camera.eyeY}<br>
+        eyeZ: ${camera.eyeZ}<br><br>
+
+        centerX: ${camera.centerX}<br>
+        centerY: ${camera.centerY}<br>
+        centerZ: ${camera.centerZ}<br><br>
+
+        upX: ${camera.upX}<br>
+        upY: ${camera.upY}<br>
+        upZ: ${camera.upZ}<br><br>
+      `;
     }
 
-    if (debug.DOMElements.canvasOverlay === undefined) {
-      debug.DOMElements.canvasOverlay = document.createElement("canvas-overlay" );
-
-      document
-        .querySelector("main")
-        .appendChild( debug.DOMElements.canvasOverlay )
-    }
-
-    const camera = sketch.camera;
-  
-    debug.DOMElements.canvasOverlay.innerHTML = `camera
-      eyeX: ${camera.eyeX}<br>
-      eyeY: ${camera.eyeY}<br>
-      eyeZ: ${camera.eyeZ}<br><br>
-
-      centerX: ${camera.centerX}<br>
-      centerY: ${camera.centerY}<br>
-      centerZ: ${camera.centerZ}<br><br>
-
-      upX: ${camera.upX}<br>
-      upY: ${camera.upY}<br>
-      upZ: ${camera.upZ}<br><br>
-    `;
+    debug.createElement( "main", "canvas-overlay", createContent, !sketch.camera)
   }
-  
 };
 
 export default debug;
