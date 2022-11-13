8d712eeb4902f7bb1626bf9f9f345f343fb61dc7
Steeve Pommier <CostardRouge@users.noreply.github.com>
2022-11-13 16:55:08 +0100

    ok

--- utils/colors.js
+++ utils/colors.js
@@ -1,9 +1,9 @@
 const colors = {
-  rainbow: ({hueOffset = 0, hueIndex, opacityFactor = 1, min = 0, max = 360}) => (
+  rainbow: ({hueOffset = 0, hueIndex, opacityFactor = 1, min = 0, max = 360, easingFunction}) => (
     color(
-      map(sin(hueOffset+hueIndex), -1, 1, min, max) / opacityFactor,
-      map(cos(hueOffset-hueIndex), -1, 1, max, min) / opacityFactor,
-      map(sin(hueOffset+hueIndex), -1, 1, max, min) / opacityFactor
+      map((easingFunction ?? sin)(hueOffset+hueIndex), -1, 1, min, max) / opacityFactor,
+      map((easingFunction ?? cos)(hueOffset-hueIndex), -1, 1, max, min) / opacityFactor,
+      map((easingFunction ?? sin)(hueOffset+hueIndex), -1, 1, max, min) / opacityFactor
     )
   ),
   darkBlueYellow: ({hueOffset = 0, hueIndex, opacityFactor = 1, min = 0, max = 360}) => (
