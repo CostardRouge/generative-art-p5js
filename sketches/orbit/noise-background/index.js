a1eeaab85b55f0cb8a4b3f1267cf7ea094c80ac4
CostardRouge <CostardRouge@users.noreply.github.com>
2022-11-24 23:40:51 +0000

    Deploying to gh-pages from @ CostardRouge/generative-art-p5js@654d55a88b30b58419e7e6ccf54fe8ae37c486d7 🚀

--- sketches/orbit/noise-background/index.js
+++ sketches/orbit/noise-background/index.js
@@ -4,7 +4,7 @@ sketch.setup( );
 
 const easingFunctions = Object.entries(easing)
 
-function cross({x, y}, size, amount = 2) {
+function cross_old({x, y}, size, amount = 2) {
   push();
   translate(x, y)
   const step = PI / amount;
@@ -15,11 +15,117 @@ function cross({x, y}, size, amount = 2) {
   pop();
 }
 
+function cross(options) {
+  const {
+    position = createVector(0, 0),
+    sides,
+    borderColor,
+    borderWidth,
+    backgroundColor,
+    size,
+    recursive = false,
+    depth = 3
+  } = options;
+
+  const maximumDepth = depth <= 0;
+
+  push();
+  translate(position)
+  beginShape()
+
+  stroke(borderColor)
+  strokeWeight(borderWidth)
+  fill(backgroundColor)
+
+  const step = PI / sides;
+  for (let i = 0; i < sides; i++) {
+    if (recursive && maximumDepth === false) {
+      const angle = TAU / sides * i;
+      const _size = size / (sides)
+
+      cross({
+        ...options,
+        position: createVector( size * cos(angle), size * sin(angle) ),
+        size: _size,
+        depth: depth -2
+      })
+    }
+    else {
+      rotate(step * 1)
+      line( -size, 0, size, 0 )
+
+      // point(-size, 0, size, 0)
+    }
+  }
+  endShape()
+  pop();
+}
+
+function polygon({
+  position,
+  sides,
+  borderColor,
+  borderWidth,
+  backgroundColor,
+  size
+}) {
+  push();
+  translate(position)
+  beginShape()
+
+  stroke(borderColor)
+  strokeWeight(borderWidth)
+  fill(backgroundColor)
+
+  const step = TAU / sides;
+  for (let i = 0; i < sides; i++) {
+    const angle = step * i;
+
+    vertex( size * cos(angle), size * sin(angle) )
+  }
+  endShape(CLOSE)
+  pop();
+}
+
 sketch.draw( (time, center) => {
   background(0);
 
-  const cols = 20;
-  const rows = 20;
+  const sides = animation.sequence("rows", time/1.3, [2, 3, 4, 5])
+  const depth = 2//animation.sequence("depth", time, [1, 2, 3, 4, 5])
+
+  // polygon({
+  //   position: center,
+  //   sides,//: 4,
+  //   borderColor: color(255),
+  //   borderWidth: 10,
+  //   backgroundColor: color(0),
+  //   size: 150
+  // })
+
+  // polygon({
+  //   position: center,
+  //   sides,//: 4,
+  //   borderColor: color(255),
+  //   borderWidth: 10,
+  //   backgroundColor: color(0),
+  //   size: 150
+  // })
+
+  // cross({
+  //   position: center,
+  //   sides,
+  //   borderColor: color(255),
+  //   borderWidth: 4,
+  //   backgroundColor: color(0),
+  //   size: 200,
+  //   depth,
+  //   recursive: true
+  // })
+
+  // return
+  const rows = 15//animation.sequence("rows", time, [50, 75])
+  const cols = 10//animation.sequence("cols", time, [75, 20])
+
   const gridOptions = {
     startLeft: createVector( 0, 0 ),
     startRight: createVector( width, 0 ),
@@ -35,53 +141,44 @@ sketch.draw( (time, center) => {
   grid.draw(gridOptions, (cellVector, { x, y}) => {
     const xOff = x/cols;
     const yOff = y/rows;
-    const amt = mappers.circularIndex(time+yOff-xOff, [ 0, 1, 2, 3, 4, 3, 2, 1]);
-
-    cross(cellVector, 10, amt)
-  })
-
-  translate(center);
-
-  const lerpStep = 1 / 512;
-  const H = height / 4;
-  const W = width / 4;
-
-  for (let lerpIndex = 0; lerpIndex < 1; lerpIndex += lerpStep) {
-    const easingFunction = mappers.circularIndex(lerpIndex+time, easingFunctions)[1]
-    const angle = mappers.fn(lerpIndex, 0, 1, -PI, PI);
-    let opacityFactor = mappers.fn(lerpIndex, 0, 1, 5, 1, easingFunction);
-    // opacityFactor = mappers.fn(sin(2*time+angle*2), -1, 1, 10, 1);
+    // const amt = mappers.circularIndex(time+yOff-xOff, [ 1, 2, 3, 4, 3, 2, 1]);
+    const amt = mappers.circularIndex(time+noise(yOff + time/5,xOff, time), [ 3, 4, 5, 6, 5]);
+    const ww = mappers.circularIndex(time+yOff-xOff, [ 1, 2, 3, 4, 3, 2, 1])*4;
 
     let colorFunction = mappers.circularIndex(
-      time+lerpIndex,
+      time+noise(yOff,xOff + time/5, time),
       Object.values( colors )
       // [colors.rainbow,colors.purple]
     )
-    colorFunction = colors.rainbow;
+    // colorFunction = colors.rainbow;
 
-    stroke(colorFunction({
-      hueOffset: time,
-      hueIndex: map(lerpIndex, 0, 1, -PI, PI)*4,
-      opacityFactor
-    }))
+    const cooo = colorFunction({
+      hueOffset: time+sin(y+time),
+      hueIndex: map(x, 0, cols-1, -PI, PI),
+      // opacityFactor: 1
+    });
 
-    let x = map(sin(angle+time*2), -1, 1, -W, W);
-    let y = map(cos(time/2+angle), -1, 1, -H, H);
-    const s = mappers.fn(lerpIndex, 0, 1, 250, 1, easingFunction);
-    const amt = animation.sequence("amt", time+lerpIndex, [ 1, 2, 3, 6, 9]);
-
-    // x = constrain( x, -W+s/2, W-s/2)
-    // y = constrain( y, -H+s/2, H-s/2)
-
-    strokeWeight(mappers.fn(lerpIndex, 0, 1, 100, 50));
+    stroke(cooo)
+    // strokeWeight(ww)
+    // cross_old(cellVector, 10, amt)
 
     push()
-    translate(x, y)
-    rotate(time/5+mappers.fn(lerpIndex, 0, 1, 0, PI, easingFunction))
-    cross({x:0, y:0}, s, amt)
+    translate(cellVector);
+    // rotate(time/5+x)
 
-    // stroke(255)
-    // point(x, y)
+    cross({
+      // position: cellVector,
+      sides: amt,
+      sides: animation.sequence("kl"+x+"-"+y, time+noise(yOff + time, xOff), [ 3, 4, 5, 6]),
+      borderColor: cooo,
+      borderWidth: 2,
+      backgroundColor: color(0),
+      size: 25,
+      depth: 2,
+      // recursive: true
+    })
+
     pop()
-  }
+
+  })
 });
