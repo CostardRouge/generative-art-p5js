6dbb1629474f54ff35548b9ddd76bcf600a689a9
CostardRouge <CostardRouge@users.noreply.github.com>
2022-11-28 02:15:09 +0000

    Deploying to gh-pages from @ CostardRouge/generative-art-p5js@47c8e4b738f4dc720421a9000a3d1a0747d4ea23 🚀

--- sketches/midi/midi-v1-output/index.js
+++ sketches/midi/midi-v1-output/index.js
@@ -47,7 +47,7 @@ sketch.setup(() => {
     // const myInput = WebMidi.getInputByName("IAC Driver Bus 1");
     // const myOutput = WebMidi.getOutputByName("IAC Driver Bus 1");
 
-    events.register("mousePressed", function () {
+    events.register("engine-mouse-pressed", function () {
       shapes.forEach((shape, index) => shape.play());
 
       playNote(
