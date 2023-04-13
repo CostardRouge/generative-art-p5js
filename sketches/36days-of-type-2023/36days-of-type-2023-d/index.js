d700b19cdc1699b5486161ad435f1a8e3a780c6e
CostardRouge <CostardRouge@users.noreply.github.com>
2023-04-13 16:44:55 +0000

    Deploying to gh-pages from @ CostardRouge/generative-art-p5js@9e5a4ae67087b22c21527ec6c461ee75f846ea89 🚀

---
+++ sketches/36days-of-type-2023/36days-of-type-2023-d/index.js
@@ -0,0 +1,193 @@
+import { sketch, string, mappers, easing, animation, colors, cache, grid } from './utils/index.js';
+
+const easingFunctions = Object.entries( easing );
+
+sketch.setup( undefined, { type: 'webgl'});
+// sketch.setup( undefined );
+// sketch.setup();
+
+function hl(y) {
+  line(0, y, width, y)
+}
+
+function vl(x) {
+  line(x, 0, x, height)
+}
+
+function getTextPoints({ text, size, font, position, sampleFactor, simplifyThreshold }) {
+  const fontFamily = font.font?.names?.fontFamily?.en;
+  const textPointsCacheKey = cache.key(text, fontFamily, "text-points")
+
+  return cache.store( textPointsCacheKey, () => {
+    const textPoints = font.textToPoints(text, position.x, position.y, size, {
+      sampleFactor, simplifyThreshold
+    });
+
+    const xMin = textPoints.reduce((a, {x}) => Math.min(a, x), Infinity);
+    const xMax = textPoints.reduce((a, {x}) => Math.max(a, x), -Infinity);
+    const xCenter = (xMax/2)+(xMin/2)
+
+    const yMin = textPoints.reduce((a, {y}) => Math.min(a, y), Infinity);
+    const yMax = textPoints.reduce((a, {y}) => Math.max(a, y), -Infinity);
+    const yCenter = (yMax/2)+(yMin/2)
+
+    return textPoints.map( ({x, y}) => {
+      const testPointVector = createVector(x, y);
+
+      testPointVector.add((position.x-xCenter),(position.y-yCenter))
+
+      return testPointVector;
+    })
+  });
+}
+
+function lerpPoints(from, to, amount, fn) {
+  return to.map( (point, index) => {
+    const targetIndex = ~~map(index, 0, to.length-1, 0, from.length-1, true);
+
+    return p5.Vector.lerp( to[index], from[targetIndex], amount )
+  })
+
+  return from.map( (point, index) => {
+    const fromIndex = map(index, 0, 1, 0, from.length-1);
+    const targetIndex = ~~map(index, 0, from.length-1, 0, to.length-1, true);
+
+    return p5.Vector.lerp( from[index], to[targetIndex], amount )
+  })
+}
+
+function drawGridCell(_x, _y, w, h, cols, rows, drawer) {
+  const xSize = w / cols;
+  const ySize = h / rows;
+
+  for (let x = 0; x <= cols; x++) {
+    for (let y = 0; y <= rows; y++) {
+      drawer?.(_x + x*xSize, _y + y*ySize, xSize, ySize)
+    }
+  }
+}
+
+function cross( x, y , size) {
+  line(x - size/2, y -size/2, x + size/2, y +size/2)
+  line(x + size/2, y -size/2, x - size/2, y +size/2)
+}
+
+function drawGrid(cols, time) {
+  const rows = cols*height/width;
+
+  const gridOptions = {
+    startLeft: createVector( -width/2, -height/2 ),
+    startRight: createVector( width/2, -height/2 ),
+    endLeft: createVector( -width/2, height/2 ),
+    endRight: createVector( width/2, height/2 ),
+    rows,
+    cols: ~~cols,
+    centered: true
+  }
+
+  const W = width / cols;
+  const H = height / rows;
+
+  noFill()
+  strokeWeight(2)
+
+  grid.draw(gridOptions, (cellVector, { x, y}) => {
+    const n = noise(x/cols+time, y/rows, time)*4;
+
+    drawGridCell(
+      cellVector.x-W/2,
+      cellVector.y-H/2,
+      W,
+      H,
+      ~~n,
+      ~~n,
+      ( x, y, w, h ) => {
+        rect(x, y, w, h )
+        cross(x, y, 30)
+      }
+    )
+  })
+}
+
+sketch.draw( (time, center) => {
+  background(0);
+
+  push()
+  translate(0, 0, -150)
+  stroke(16, 16, 64)
+  drawGrid(5, time)
+  pop()
+
+  rotateY(mappers.fn(sin(time*2), -1, 1, -PI, PI, easing.easeInSine)/18)
+  rotateX(mappers.fn(cos(time), -1, 1, -PI, PI)/9)
+
+  const size = 500;
+  const scale = 2.25;
+
+  const sampleFactor = 0.05;
+  const simplifyThreshold = 0;
+  const letterPosition = createVector(0, 0)
+
+  const currentLetterPoints = getTextPoints({
+    text: "d",
+    position: letterPosition,
+    size,
+    font: string.fonts.serif,
+    sampleFactor,
+    simplifyThreshold
+  })
+
+  const nextLetterPoints = getTextPoints({
+    text: "D",
+    position: letterPosition,
+    size,
+    font: string.fonts.serif,
+    sampleFactor,
+    simplifyThreshold
+  })
+
+  const depthSteps = 50;
+  const depthStart = 0;
+  const depthEnd = 500;
+
+  for (let z = 0; z < depthSteps; z++) {
+    push()
+
+    translate(
+      0,
+      0, 
+      lerp( depthStart, depthEnd, z/depthSteps )
+    )
+  
+    const points = lerpPoints(currentLetterPoints, nextLetterPoints, z/depthSteps )
+
+    points.forEach( ({x, y}) => {
+      const hue = noise(
+        x/100,
+        y/100,
+        z/depthSteps-time/2
+      )
+      
+      const opacityFactor = mappers.fn(sin(z/(depthSteps/3)+time*3), -1, 1, 20, 1, easing.easeInQuint);
+
+      if (opacityFactor > 15) {
+        return
+      }
+
+      stroke(colors.rainbow({
+        hueOffset: 0,
+        hueIndex: mappers.fn(hue, 0, 1, -PI, PI)*4,
+        opacityFactor
+      }))
+
+      point(
+        x * scale,
+        y * scale
+      )
+    })
+
+    pop()
+  }
+
+  orbitControl();
+});
