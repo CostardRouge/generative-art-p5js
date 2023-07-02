024be0449d475171fdeda1978e449e6c42a59999
Steeve Pommier <CostardRouge@users.noreply.github.com>
2023-07-02 05:19:26 +0200

    cube and cubes

--- sketches/3d-stuff/cube/index.js
+++ sketches/3d-stuff/cube/index.js
@@ -1,38 +1,113 @@
-import { sketch, string, mappers, easing, events, colors, cache, grid } from './utils/index.js';
+import { sketch, options, iterators, converters, debug, events, string, mappers, easing, animation, colors, cache, grid } from './utils/index.js';
 
-//sketch.setup( undefined);
-sketch.setup( undefined, { type: 'webgl'});
+function getTextPoints({ text, size, font, position, sampleFactor, simplifyThreshold }) {
+  const fontFamily = font.font?.names?.fontFamily?.en;
+  const textPointsCacheKey = cache.key(
+    "text-points",
+    fontFamily,
+    text,
+    size,
+    sampleFactor,
+    simplifyThreshold
+  )
 
-events.register("engine-window-preload", () => {
-  cache.store("image", () => loadImage( "img.png" ))
-});
+  return cache.store( textPointsCacheKey, () => {
+    const textPoints = font.textToPoints(text, position.x, position.y, size, {
+      sampleFactor, simplifyThreshold
+    });
 
-function drawGrid(xCount, yCount, handler) {
+    const xMin = textPoints.reduce((a, {x}) => Math.min(a, x), Infinity);
+    const xMax = textPoints.reduce((a, {x}) => Math.max(a, x), -Infinity);
+    const xCenter = (xMax/2)+(xMin/2)
+
+    const yMin = textPoints.reduce((a, {y}) => Math.min(a, y), Infinity);
+    const yMax = textPoints.reduce((a, {y}) => Math.max(a, y), -Infinity);
+    const yCenter = (yMax/2)+(yMin/2)
+
+    return ([
+      textPoints.map( ({x, y}) => {
+        const testPointVector = createVector(x, y);
+  
+        testPointVector.add((position.x-xCenter),(position.y-yCenter))
+  
+        return testPointVector;
+      }),
+      [
+        xMin-xCenter,
+        yMin-yCenter,
+        xMax-xMin,
+        yMax-yMin,
+      ],
+      textPointsCacheKey
+    ])
+  });
+}
+
+function getAlphaFromMask({ position: { x, y }, maskPoints, distance = 0.015}) {
+  const normalizedPosition = createVector(
+    map(x, -width/2, width/2, 0, 1),
+    map(y, -height/2, height/2, 0, 1)
+  );
+
+  return maskPoints.reduce( ( result, pointPosition ) => {
+    if (true === result) {
+      return result;
+    }
+
+    const normalizedPointPosition = createVector(
+      map(pointPosition.x, -width /2, width/2, 0, 1),
+      map(pointPosition.y, -height /2, height/2, 0, 1)
+    );
+
+    const d = normalizedPointPosition.dist(normalizedPosition);
+
+    return Math.max(
+      result,
+      d > 0 && d < distance
+    );
+  }, 0);
+}
+
+function createGridAlphaPoints(gridOptions, textPointsMatrix) {
+  return cache.store( `alpha-points-matrix`, () => {
+    const alphaPoints = [];
+
+    grid.draw(gridOptions, position => {
+      const alphaLayers = [];
+
+      for ( const [ points ] of textPointsMatrix ) {
+        const alpha = getAlphaFromMask({
+          position,
+          maskPoints: points
+        })
+    
+        alphaLayers.push( alpha );
+      }
+
+      alphaPoints.push( {
+        position,
+        layers: alphaLayers
+      } );
+    });
+
+    return alphaPoints;
+  });
+}
+
+function drawGrid(xCount, yCount){
   const xSize = width / xCount;
   const ySize = height / yCount;
 
-
+  rectMode(CENTER);
 
   const offset = 0;
 
-  for (let x = offset; x < xCount - offset; x++) {
-    for (let y = offset; y < yCount - offset; y++) {
-      stroke(255)
+  for (let x = offset; x <= xCount - offset; x++) {
+    for (let y = offset; y <= yCount - offset; y++) {
       hl(y * ySize)
       vl(x * xSize)
-
-      handler?.(
-        x * xSize+xSize/2,
-        y * ySize+ySize/2,
-        xSize,
-        ySize
-      )
     }
   }
-
-  stroke(255)
-  hl(height)
-  vl(width)
 }
 
 function hl(y) {
@@ -43,177 +118,287 @@ function vl(x) {
   line(x, 0, x, height)
 }
 
-sketch.draw( (time, center) => {
-  background(0);
-
-  const planesCount = 5;
-  const gap = 100;
-  const depth = planesCount
-
-  //rotateX(time)
-  //rotateY(time)
-  //rotateZ(time)
-
-  const hueSpeed = {
-    x: 0,
-    y: 0,
-    //z: -time/2,
+class Cube {
+  moves = {
+    x: {
+      LEFT_ARROW: -1,
+      RIGHT_ARROW: 1
+    },
+    y: {
+      UP_ARROW: -1,
+      DOWN_ARROW: 1
+    }
   }
 
-  const noiseSpeed = {
-    x: 0,
-    y: 0,
-    z: -time/2,
+  constructor(options) {
+    Object.assign(this, options);
+
+    this.animation = {
+      x: 1,
+      y: 1
+    };
+    this.next = this.position.copy();
+    this.rotation = createVector(0, 0);
   }
 
-  push()
-    rectMode(CENTER);
-    strokeWeight(2)
-    translate(-center.x, -center.y)
+  draw() {
+    const {
+      size, cols, rows,
+      position: { x, y },
+      rotation: { x: rX, y: rY },
+      next: { x: nextX, y: nextY },
+      animation: { x: xAnimation, y: yAnimation },
+    } = this;
 
-    for (let z = 0; z < planesCount; z++) {
-      translate(0, 0, -gap)
-      stroke(255)
+    const W = width / cols;
+    const H = height / rows;
 
-      drawGrid(12, 12, (x, y, w, h) => {
-        push()
-        translate(x, y, gap/2-gap)
-
-        const d = noise(
-          x+noiseSpeed.x,
-          y+noiseSpeed.y,
-          z/100+noiseSpeed.z
-        )
-        const hue = noise(
-          x/width+hueSpeed.x,
-          y/height+hueSpeed.y,
-          z/depth+hueSpeed.z
-        )
-  
-        if (d > 0.5) {
-          const coco = colors.rainbow({
-            hueOffset: 0,
-            hueIndex: map(hue, 0, 1, -PI, PI)*2,
-            opacityFactor: map(hue, 0, 1, 2.1, 1 ),
-          })
-
-          fill(0)
-          fill(coco)
-          stroke(255)
-          // rect(x, y, w-10, h-10)
-          box(w-10, h-10, gap-10)
-        }
-        pop()
-
-      })
-    }
-  pop()
-
-  return orbitControl()
-
-  // push()
-  //   noFill( )
-  //   stroke(64, 64, 128)
-  //   strokeWeight(2)
-  //   drawBackgroundPattern(time, 10, 15);
-  // pop()
-
-  const speed = time//2;
-  const duration = 1;
-  const word = "*"//"÷æ«$©®…ø†¥ø&%+#$@!*";
-  const size = 950;
-  const font = string.fonts.serif;
-  const currentLetter = mappers.circularIndex(speed, word)
-  const nextLetter = mappers.circularIndex(speed+1, word)
-
-  const sampleFactor = 0.8//mappers.circularIndex(time, [0.5, 0.1, 0.25]);
-  const simplifyThreshold = 0//mappers.circularIndex(time*5, [0.001, 0]);
-
-  const currentLetterPoints = getTextPoints({
-    text: currentLetter,
-    position: createVector(0, 0),
-    size,
-    font,
-    sampleFactor,
-    simplifyThreshold
-  })
-
-  // const nextLetterPoints = getTextPoints({
-  //   text: nextLetter,
-  //   position: createVector(0, 0),
-  //   size,
-  //   font,
-  //   sampleFactor,
-  //   simplifyThreshold
-  // })
-
-  stroke(0, 0, 255)
-  strokeWeight(4)
-
-  for (let i = 0; i < currentLetterPoints.length; i++) {
-    const progression = map(i, 0, currentLetterPoints.length-1, 0, 1);
-    // const targetIndex = ~~map(i, 0, currentLetterPoints.length-1, 0, nextLetterPoints.length-1, true);
-
-    // const { x, y } = animation.ease({
-    //   values: [ currentLetterPoints[i], nextLetterPoints[targetIndex] ],
-    //   currentTime: speed,
-    //   duration: duration,
-    //   easingFn: easing.easeInOutExpo,
-    //   // easingFn: easing.easeInOutElastic,
-    //   lerpFn: p5.Vector.lerp,
-    //   startIndex: 0,
-    //   endIndex: 1
-    // })
-
-    const { x, y } = currentLetterPoints[i]
-
-    let colorFunction = mappers.circularIndex(time/4+progression, [
-      colors.rainbow,
-      //colors.test
-    ]);
-
-    if (i == currentLetterPoints.length-1) {
-      //colorFunction = colors.rainbow;
-    }
-
-    noFill()
-    stroke(colorFunction({
-      hueOffset: time,
-      hueIndex: mappers.fn(progression, 0, 1, -PI, PI, easing.easeInCubic),
-      opacityFactor: map(sin(-progression*100+time*2), -1, 1, 3, 1),
-      opacityFactor: mappers.fn(
-        sin(-progression*100+time*2),
-        -1,
-        1,
-        mappers.fn(cos(progression*50+time*3), -1, 1, 5, 1, easing.easeInCubic),
-        1,
-        easing.easeInSine
-      ),
-    }))
-    
-    let amt = mappers.circularIndex(time/2+progression, [, 2, 3, 5, 7]);
-    amt = mappers.circularIndex(time/6+progression, [,5]);
-    const size = mappers.circularIndex(time/3+progression, [30, 50]);
-
-// rotateX(mappers.fn(cos(t+x/cols), -1, 1, -PI, PI, easing.easeInOutQuart))
-    const a = mappers.fn(sin(time+progression), -1, 1, -PI, PI, easing.easeInOutSine)/3
+    const position = createVector(
+      size/2 + (x * W) - width/2,
+      size/2 + (y * H) - height/2,
+      size/2,
+    )
 
     push()
-    translate(
-      x*2,
-      y*2
-    )
-    rotate(-time+progression);
-    //rotate(progression*a*amt);
+    translate(position)
 
+    rotateX(rY)
+    rotateY(rX)
 
-    const img = cache.get("image");
+    stroke(255, 0, 0)
+    strokeWeight(2)
 
-    //image(img, 0, 0, size, size )
-
-    flower(size, amt)
-    //huriken(size, 7)
+    box(size, size, size)
     pop()
   }
 
+  update(time) {
+    const {
+      cols, rows, position, next, animation, animationSpeed,
+    } = this;
+
+    if ( animation.x < 1 ) {
+      this.animation.x += animationSpeed;
+      this.position.x = lerp( position.x, next.x, constrain(
+        animation.x, 0, 1
+      ) )
+
+      const rDirection = position.x >= next.x ? -PI/2 : PI/2
+
+      this.rotation.x = lerp( 0, rDirection, constrain(
+        animation.x, 0, 1
+      ) )
+    }
+
+    if ( animation.y < 1 ) {
+      this.animation.y += animationSpeed;
+      this.position.y = lerp( position.y, next.y, constrain(
+        animation.y, 0, 1
+      ) )
+
+      const rDirection = position.y >= next.y ? PI/2 : -PI/2
+
+      this.rotation.y = lerp( 0, rDirection, constrain(
+        animation.y, 0, 1
+      ) )
+    }
+
+    if ( animation.y >= 1 ) {
+      if (keyIsDown(UP_ARROW)) {
+        next.y = constrain(position.y - 1, 0, rows -1)
+        this.animation.y = 0;
+      }
+
+      if (keyIsDown(DOWN_ARROW)) {
+        next.y = constrain(position.y + 1, 0, rows -1)
+        this.animation.y = 0;
+      }
+    }
+
+    if ( animation.x >= 1 ) {
+      if (keyIsDown(LEFT_ARROW)) {
+        next.x = constrain(position.x - 1, 0, cols -1)
+        this.animation.x = 0;
+      }
+
+      if (keyIsDown(RIGHT_ARROW)) {
+        next.x = constrain(position.x + 1, 0, cols -1)
+        this.animation.x = 0;
+      }
+    }
+  }
+}
+
+sketch.setup( undefined , { type: 'webgl'});
+
+// events.register("post-setup", function () {
+//   events.register("engine-on-key-typed", function () {
+//     console.log(events.handle("engine-get-key-typed"));
+//     if (!events.handle("engine-get-key-typed").includes(" ")) {
+//       return;
+//     }
+//   });
+// });
+
+sketch.draw( (time, center) => {
+  background(0);
+
+  const cols = 10;
+  const rows = cols*height/width;
+  const cellSize = width/cols;
+
+  rotateX(PI/4)
+
+  stroke(255)
+  strokeWeight(1)
+
+  const cubes = cache.store("cubes", () => {
+    return [
+      new Cube({
+        position: createVector(0, 0),
+        animationSpeed: 0.1,
+        size: cellSize,
+        cols, rows
+      })
+    ]
+  });
+
+  push()
+  translate(-center.x, -center.y)
+  drawGrid(cols, rows, -time)
+  pop()
+
+  cubes.forEach( cube => cube.update(time) );
+  cubes.forEach( cube => cube.draw() );
+  
+  return orbitControl();
+
+  const size = (width);
+  const sampleFactor = 1/10;
+  const simplifyThreshold = 0;
+
+  const gridOptions = {
+    startLeft: createVector( -width/2, -height/2 ),
+    startRight: createVector( width/2, -height/2 ),
+    endLeft: createVector( -width/2, height/2 ),
+    endRight: createVector( width/2, height/2),
+    rows,
+    cols,
+    centered: true
+  }
+
+  const fonts = [
+    string.fonts.martian,
+    // string.fonts.tilt,
+    string.fonts.multicoloure,
+    string.fonts.openSans,
+    string.fonts.sans,
+    string.fonts.serif
+  ]
+
+  const textPointsMatrix = fonts.map( font => (
+    getTextPoints({
+      text: "9",
+      position: createVector(0, 0),
+      size,
+      font,
+      sampleFactor,
+      simplifyThreshold
+    })
+  ))
+
+  const alphaPoints = createGridAlphaPoints(
+    gridOptions,
+    textPointsMatrix
+  )
+
+  const rotationMax = PI*2
+  const generalAnimationTime = time/2
+
+  const {
+    x: rX,
+    y: rY,
+    //z: rZ
+  } = animation.ease({
+    values: [
+      createVector(), 
+      createVector(0, rotationMax),
+      createVector(rotationMax, rotationMax),
+      createVector(rotationMax),
+    ],
+    currentTime: generalAnimationTime,
+    duration: 1,
+    lerpFn: p5.Vector.lerp,
+    easingFn: easing.easeInOutExpo,
+    // easingFn: easing.easeInOutElastic,
+    // easingFn: easing.easeInOutCirc,
+  })
+
+  rotateX(rX)
+  rotateY(rY)
+
+  // const finalPoints = alphaPoints
+
+  alphaPoints.forEach( ( { layers, position }, index ) => {
+    const layer = mappers.circularIndex(
+      generalAnimationTime+1/2,
+      layers
+    )
+    // const layer = animation.ease({
+    //   values: layers,
+    //   currentTime: generalAnimationTime+1/2,
+    //   duration: 1,
+    //   easingFn: easing.easeInOutExpo
+    // })
+
+    if (!layer) {
+      return;
+    }
+
+    const switchIndex = generalAnimationTime*2+(
+      +index/alphaPoints.length/5
+      +position.x/cols/100
+      +position.y/rows/100
+    )
+
+    const hue = noise(
+      position.x/cols + (
+        +map(sin(time), -1, 1, 0, 1)
+      ),
+      position.y/rows + (
+        +map(cos(time), -1, 1, 0, 1)
+      )
+    )
+
+    const tint = colors.rainbow({
+      hueOffset: time,
+      hueIndex: map(hue, 0, 1, -PI, PI)*2,
+      opacityFactor: 1.5
+    })
+
+    const { levels: [ red, green, blue ] } = tint;
+
+    push()
+
+    const w = cellSize//-2
+    const h = cellSize//-2
+    const d = cellSize*20
+
+    translate( position )
+
+    const fillAlpha = animation.ease({
+      values: [ 240, 0 ],
+      currentTime: switchIndex,
+      duration: 1,
+      easingFn: easing.easeInOutExpo,
+    });
+
+    fill( red, green, blue, fillAlpha )
+    stroke( red, green, blue, 200 )
+    box(w, h, -d)
+
+    pop()
+  } );
+
+  orbitControl();
 });
