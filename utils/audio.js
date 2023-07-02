7eab167f8667b1075733bdb469a80c8679102374
Steeve Pommier <CostardRouge@users.noreply.github.com>
2023-07-02 04:38:09 +0200

    opengraph images

--- utils/audio.js
+++ utils/audio.js
@@ -1,4 +1,4 @@
-import { events } from './index.js';
+import { events } from "./index.js";
 
 const HISTORY_BUFFER = 20;
 
@@ -71,11 +71,11 @@ const ranges = {
     smooth: 0,
     count: 0,
     countDeltaTime: 0,
-    amplified: undefined
-  }
+    amplified: undefined,
+  },
 };
 
-const rangeNames = Object.keys( ranges );
+const rangeNames = Object.keys(ranges);
 
 const audio = {
   capture: {
@@ -86,75 +86,97 @@ const audio = {
     historyBufferSize: HISTORY_BUFFER,
     smoothness: 0.67,
     bins: 2048,
-    setup: async (smoothness = audio.capture.smoothness, bins = audio.capture.bins) => {
-      userStartAudio()
+    setup: async (
+      smoothness = audio.capture.smoothness,
+      bins = audio.capture.bins
+    ) => {
+      userStartAudio();
 
       audio.capture.smoothness = smoothness ?? audio.capture.smoothness;
       audio.capture.bins = bins ?? audio.capture.bins;
 
       audio.capture.audioIn = new p5.AudioIn();
       audio.capture.fft = new p5.FFT(smoothness, bins);
-  
+
       audio.capture.audioIn.start();
       audio.capture.fft.setInput(audio.capture.audioIn);
-  
+
       // console.log({
       //   AudioIn: audio.capture.audioIn,
       //   FFT: audio.capture.fft
       // });
 
-      events.register("pre-draw", audio.capture.energy.monitor );
+      events.register("pre-draw", audio.capture.energy.monitor);
     },
     energy: {
+      // spectrum mapping
+      map: (xNormalized, yNormalized, normalize = true) => {
+        const historyBufferIndex = ~~(
+          (audio.capture.historyBufferSize - 1) *
+          yNormalized
+        );
+
+        const bins = audio.capture.history?.spectrum?.[historyBufferIndex];
+        const binIndex = ~~((audio.capture.bins - 1) * xNormalized);
+        const energy = bins?.[binIndex];
+
+        if (normalize) {
+          return energy / 255;
+        }
+
+        return energy;
+      },
       average: (attribute = "raw") => {
-        const result = (
-          rangeNames.reduce( (average, rangeName, _, {length}) => {
-            return average + (ranges[rangeName][attribute] / length)
-          }, 0)
+        const result = rangeNames.reduce(
+          (average, rangeName, _, { length }) => {
+            return average + ranges[rangeName][attribute] / length;
+          },
+          0
         );
 
         return isNaN(result) ? 0 : result;
       },
-      byIndex: (index, attribute = "smooth") => (
-        ranges[rangeNames[ index ]]?.[attribute] ?? 0
-      ),
-      byCircularIndex: (index, attribute = "smooth") => (
-        ranges[rangeNames[ index % rangeNames.length ]]?.[attribute] ?? 0
-      ),
-      byName: (name, attribute = "smooth") => (
-        ranges[name]?.[attribute] ?? 0
-        ),
-        getSpectrumHistoryFromVector: (xNormalized, yNormalized, attribute = "smooth") => {
-          const binIndex = floor((audio.capture.bins -1) * xNormalized)
-          const historyBufferIndex = floor((audio.capture.historyBufferSize-1) * yNormalized)
-      
-          const line = audio.capture.history?.spectrum[historyBufferIndex];
-  
-          return line?.[binIndex]
-        },
-        getRangeHistoryFromVector: (normalizedIndex, rangeName, attribute = "smooth") => {
-          const historyBufferIndex = floor((audio.capture.historyBufferSize-1) * normalizedIndex)
-      
-          return audio.capture.history?.ranges[rangeName]?.[historyBufferIndex]?.[attribute]
-        },
+      byIndex: (index, attribute = "smooth") =>
+        ranges[rangeNames[index]]?.[attribute] ?? 0,
+      byCircularIndex: (index, attribute = "smooth") =>
+        ranges[rangeNames[index % rangeNames.length]]?.[attribute] ?? 0,
+      byName: (name, attribute = "smooth") => ranges[name]?.[attribute] ?? 0,
+      getRangeHistoryFromVector: (
+        normalizedIndex,
+        rangeName,
+        attribute = "smooth"
+      ) => {
+        const historyBufferIndex = ~~(
+          (audio.capture.historyBufferSize - 1) *
+          normalizedIndex
+        );
+
+        return audio.capture.history?.ranges[rangeName]?.[historyBufferIndex]?.[
+          attribute
+        ];
+      },
       recordHistory: () => {
-        if ( false === audio.capture.audioIn?.enabled ) {
+        if (false === audio.capture.audioIn?.enabled) {
           return;
         }
 
         if (undefined === audio.capture.history) {
           audio.capture.history = {
-            spectrum: new Array(audio.capture.historyBufferSize).fill(undefined),
-            waveform: new Array(audio.capture.historyBufferSize).fill(undefined),
-            ranges: (
-              rangeNames.reduce( (history, rangeName, _, {length}) => {
-                return {
-                  ...history,
-                  [rangeName]: new Array(audio.capture.historyBufferSize).fill(undefined)
-                }
-              }, {})
-            )
-          }
+            spectrum: new Array(audio.capture.historyBufferSize).fill(
+              undefined
+            ),
+            waveform: new Array(audio.capture.historyBufferSize).fill(
+              undefined
+            ),
+            ranges: rangeNames.reduce((history, rangeName, _, { length }) => {
+              return {
+                ...history,
+                [rangeName]: new Array(audio.capture.historyBufferSize).fill(
+                  undefined
+                ),
+              };
+            }, {}),
+          };
         }
 
         // HISTORY FOR SPECTRUM
@@ -183,16 +205,16 @@ const audio = {
         }
       },
       monitor: () => {
-        if ( false === audio.capture.audioIn?.enabled ) {
+        if (false === audio.capture.audioIn?.enabled) {
           return;
         }
-      
+
         audio.capture.fft.analyze();
-      
+
         for (const rangeName in ranges) {
           const range = ranges[rangeName];
 
-          range.raw = audio.capture.fft.getEnergy( ...range.frequencies ) / 255;
+          range.raw = audio.capture.fft.getEnergy(...range.frequencies) / 255;
           range.amplified = range.raw * range.amplifier;
 
           // console.log({
@@ -202,14 +224,14 @@ const audio = {
           //   index: rangeNames.indexOf( rangeName )
           // });
 
-          if ( range.raw >= range.threshold ) {
+          if (range.raw >= range.threshold) {
             //range.smooth = lerp( range.smooth, range.amplified, 0.67 );
             range.smooth = range.raw;
             range.threshold = range.raw;
 
             const currentTime = millis();
 
-            if ( currentTime > ( range.countDeltaTime + 500) ) {
+            if (currentTime > range.countDeltaTime + 500) {
               range.count += 1;
               range.countDeltaTime = millis();
             }
@@ -218,36 +240,36 @@ const audio = {
           // if ( range.raw <= 0.2) {
           //   range.smooth = lerp( range.smooth, 0, 0.67 );
           // }
-          range.smooth = lerp( range.smooth, 0, 0.067 );
-          range.threshold = lerp( range.threshold, 0, 0.09 );
+          range.smooth = lerp(range.smooth, 0, 0.067);
+          range.threshold = lerp(range.threshold, 0, 0.09);
         }
       },
-      draw: ( spectrum = true, waveform = true ) => {
-        if ( false === audio.capture.audioIn?.enabled ) {
+      draw: (spectrum = true, waveform = true) => {
+        if (false === audio.capture.audioIn?.enabled) {
           return;
         }
 
-        if ( true === waveform ) {
+        if (true === waveform) {
           const wf = audio.capture.fft.waveform();
           const w = width / audio.capture.bins;
-          const h = 50
+          const h = 50;
 
-          stroke('blue')
-          fill('red')
+          stroke("blue");
+          fill("red");
           for (let i = 0; i < wf.length; i++) {
-            const y = map(wf[ i ], -1, 1, h, 0);
+            const y = map(wf[i], -1, 1, h, 0);
 
-            rect(i * w, h - y/2, w, y );
+            rect(i * w, h - y / 2, w, y);
           }
         }
 
-        if (true === spectrum ) {
+        if (true === spectrum) {
           const wf = audio.capture.fft.analyze();
 
-          stroke('red')
-          fill('blue')
+          stroke("red");
+          fill("blue");
           for (let i = 0; i < wf.length; i++) {
-            const h = map(wf[ i ], 0, 255, 0, height/4);
+            const h = map(wf[i], 0, 255, 0, height / 4);
             const x = map(i, 0, wf.length, 0, width);
 
             // line(i, height, i, y );
@@ -255,11 +277,11 @@ const audio = {
             rect(x, height, width / wf.length, -h);
           }
         }
-      }
-    }
-  }
+      },
+    },
+  },
 };
 
-window.audio = audio
+window.audio = audio;
 
 export default audio;
--- utils/midi.js
+++ utils/midi.js
@@ -3,70 +3,89 @@ import { events } from './index.js';
 const MONITORING_BUFFER = 60;
 
 const midi = {
-  inputs: [],
-  outputs: [],
+  inputs: {},
+  outputs: {},
   monitoring: { },
   byCircularIndex: (index, attribute = "on") => {
     const noteIdentifiers = Object.keys( midi.monitoring );
 
     return midi.monitoring[noteIdentifiers[ index % noteIdentifiers.length ]]?.[attribute];
   },
-  setup: () => {
+  loadScripts: async () => {
+    await loadScript("libraries/webmidi.iife.js");
+  },
+  setup: async () => {
+    await midi.loadScripts()
+
     WebMidi.enable()
       .then( () => {
         if (WebMidi.inputs.length < 1) {
           return console.log("No device detected.");
         }
-    
+
         WebMidi.inputs.forEach((input, index) => {
-          console.log(`INPUT: ${index}: ${input.name}`);
+          const { id } = input;
 
-          midi.inputs.push(input);
+          midi.inputs[ id ] = input;
 
-          input.addListener("noteon", (e) => {
-            const { identifier }  = e.note;
+          console.log(`< INPUT: ${index}: ${input.name} (${id})`);
+        })
 
-            if ( undefined === midi.monitoring[ identifier ] ) {
-              midi.monitoring[ identifier ] = {
-                history: new Array(MONITORING_BUFFER).fill(0)
-              }
-            }
+        WebMidi.outputs.forEach((output, index) => {
+          const { id } = output;
 
-            const noteHistory = midi.monitoring[ identifier ].history;
+          midi.outputs[ id ] = output;
 
-            // console.log(midi.monitoring[ identifier ]);
-
-            midi.monitoring[ identifier ].history[ noteHistory.length -1 ] = identifier;
-            midi.monitoring[ identifier ].on = true;
-            midi.monitoring[ identifier ].smooth = 1;
-            // midi.monitoring[identifier ].smooth = lerp( midi.monitoring[identifier].smooth ?? 1, 1, 0.067 );
-
-
-            // console.log( "on", e.note.identifier )
-          });
-
-          input.addListener("noteoff", (e) => {
-            const { identifier }  = e.note;
-
-            if ( undefined === midi.monitoring[ identifier ] ) {
-              midi.monitoring[ identifier ] = {
-                history: new Array(MONITORING_BUFFER).fill(0)
-              };
-            }
-
-            midi.monitoring[ identifier ].on = false;
-            midi.monitoring[identifier ].smooth = lerp( midi.monitoring[identifier].smooth ?? 1, 0, 0.067 );
-
-            console.log( "off", e.note.identifier )
-          });
-        });
+          console.log(`> OUTPUT: ${index}: ${output.name} (${id})`);
+        })
     
-        WebMidi.outputs.forEach((device, index) => {
-          console.log(`OUTPUT: ${index}: ${device.name}`);
+        // WebMidi.inputs.forEach((input, index) => {
 
-          midi.outputs.push(device);         
-        });
+        //   midi.inputs.push(input);
+
+        //   input.addListener("noteon", (e) => {
+
+        //     const { identifier, name, octave }  = e.note;
+
+        //     console.log({ identifier, name, octave }, e);
+
+
+        //     if ( undefined === midi.monitoring[ identifier ] ) {
+        //       midi.monitoring[ identifier ] = {
+        //         history: new Array(MONITORING_BUFFER).fill(0)
+        //       }
+        //     }
+
+        //     const noteHistory = midi.monitoring[ identifier ].history;
+
+        //     // console.log(midi.monitoring[ identifier ]);
+
+        //     midi.monitoring[ identifier ].history[ noteHistory.length -1 ] = identifier;
+        //     midi.monitoring[ identifier ].on = true;
+        //     midi.monitoring[ identifier ].smooth = 1;
+        //     // midi.monitoring[identifier ].smooth = lerp( midi.monitoring[identifier].smooth ?? 1, 1, 0.067 );
+
+
+        //     // console.log( "on", e.note.identifier )
+        //   });
+
+        //   input.addListener("noteoff", (e) => {
+        //     const { identifier }  = e.note;
+
+        //     if ( undefined === midi.monitoring[ identifier ] ) {
+        //       midi.monitoring[ identifier ] = {
+        //         history: new Array(MONITORING_BUFFER).fill(0)
+        //       };
+        //     }
+
+        //     midi.monitoring[ identifier ].on = false;
+        //     midi.monitoring[identifier ].smooth = lerp( midi.monitoring[identifier].smooth ?? 1, 0, 0.067 );
+
+        //     console.log( "off", e.note.identifier )
+        //   });
+        // });
       })
+      .then( midi.attachListenersToInputs )
       .catch( console.error );
 
     events.register( "pre-draw", midi.monitor );
@@ -81,6 +100,12 @@ const midi = {
       // })
     });
   },
+  on: (note, handler, device = "all") => {
+    events.register( `midi-on-${note}-${device}`, handler );
+  },
+  off: (note, handler, device = "all") => {
+    events.register( `midi-off-${note}-${device}`, handler );
+  },
   monitor: () => {
     // console.log(midi.monitoring);
 
@@ -90,7 +115,45 @@ const midi = {
       midi.monitoring[identifier].smooth = lerp( midi.monitoring[identifier].smooth, 0, 0.05 );
     }
   },
-  //release, and attack ?
+  attachListenersToInputs: () => {
+    const getNoteDetails = ({
+      identifier,
+      name,
+      octave,
+      rawAttack: attack,
+      _release: release
+    }) => ({
+      identifier,
+      name,
+      octave,
+      attack,
+      release
+    })
+
+    const eventHandler = ( note, type, id) => {
+      const { identifier, name } = note;
+
+      // All
+      events.handle( `midi-${type}-${identifier}-all`,  note) // A6 -  all
+      events.handle( `midi-${type}-${name}-all`, note ) // A - all
+
+      // Specific input
+      events.handle( `midi-${type}-${identifier}-${id}`,  note) // A6 - specific device id
+      events.handle( `midi-${type}-${name}-${id}`, note ) // A - specific device id
+    }
+
+    for (const id in midi.inputs) {
+      const input = midi.inputs[ id ];
+
+      input.addListener("noteon", event => {
+        eventHandler( getNoteDetails(event.note), "on", id)
+      });
+
+      input.addListener("noteoff", event => {
+        eventHandler( getNoteDetails(event.note), "off", id)
+      });
+    }
+  },
 };
 
 export default midi;
