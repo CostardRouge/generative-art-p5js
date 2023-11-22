52af7fddca74aa27ef3d101088250a1ea3414f33
CostardRouge <CostardRouge@users.noreply.github.com>
2023-11-22 20:05:28 +0000

    Deploying to gh-pages from @ CostardRouge/generative-art-p5js@7ba5de818e9c1413410eeade8196296f3295de1d 🚀

--- sketches/trace-vectors/trace-vectors-v11-135-grid-moving/index.js
+++ sketches/trace-vectors/trace-vectors-v11-135-grid-moving/index.js
@@ -22,7 +22,9 @@ function drawGrid(xCount, yCount, color, weight = 2, offset = 0) {
 sketch.setup( undefined, { type: "webgl" } );
 
 let alphabet = Array(26).fill(undefined).map((_, index) => String.fromCharCode(index + 'a'.charCodeAt(0)))
-// alphabet = "123456".split("")
+alphabet = "123456789".split("") 
+
+let gt = 0;
 
 function drawLine( points, type ) {
   beginShape(type);
@@ -48,35 +50,71 @@ sketch.draw( ( time, center, favoriteColor ) => {
   push()
   translate(-W, -H)
   const columns = 3;
-  const rows = 3//columns * (height/width);
+  const rows = 3///columns * (height/width);
   drawGrid(columns, rows, favoriteColor, 0.5, 0)
   pop()
 
-  const positions = cache.store("positions", () => {
-    const positions = []
+  const cases = cache.store("cases", () => {
+    const cases = []
 
     for (let yy = 0; yy < rows; yy++) {
       const y = lerp(-H/(rows/2), H/(rows/2), yy/(rows-1));
-  
+
       for (let xx = 0; xx < columns; xx++) {
         const x = lerp(-W/(columns/2), W/(columns/2), xx/(columns-1));
   
-        positions.push( createVector( x, y ) )
+        cases.push( createVector( x, y ) )
       }
     }
 
-    return positions;
-  })//.reverse()
+    return cases;
+  })
 
-  
-  // drawLine(positions, POINTS)
+  push()
+  cases.forEach( ({x, y}, index) => {
+    string.write(index+1, x, y, {
+      center: true,
+      size: 14,
+      stroke: 255,
+      fill: favoriteColor,
+      font: string.fonts.martian
+    })
+  })
+
+  pop()
+
+  const positions = cache.store("positions", () => {
+     // return positions//.sort( ({x, y}, {x: xx, y: yy}) => x-xx - y-yy);
+
+    // return [
+    //   cases[0],
+    //   cases[2],
+    //   cases[6],
+    //   cases[8],
+    // ]
+
+    // return [
+    //   cases[1],
+    //   cases[2],
+    //   cases[6],
+    //   cases[8],
+    // ]
+
+    return [
+      cases[1],
+      cases[5],
+      cases[7],
+      cases[3],
+    ]
+  })
   stroke(96)
   strokeWeight(2)
+  // drawLine(positions, 2)
 
-  beginShape()
-  iterators.vectors(positions, ({x, y }) => {
-    point(x, y)
-  }, 0.05)
+  // beginShape()
+  // iterators.vectors(positions, ({x, y }) => {
+  //   point(x, y)
+  // }, 0.05)
 
   // iterators.vectors([positions[0], positions[positions.length -1]], ({x, y }) => {
   //   point(x, y)
@@ -90,15 +128,10 @@ sketch.draw( ( time, center, favoriteColor ) => {
   const end = [];
   const middle = [];
 
-  const timeSpeed = animation.ease({
-    values: [1, 2, 3],
-    duration: 1,
-    easingFn: easing.easeInOutCubic,
-    currentTime: time
-  }) 
+  gt += mappers.circularIndex(time, [0, 0, .0075, .015])
 
   mappers.traceVectors(
-    33,
+    5,
     ( progression ) => {
       return animation.ease({
         values: alphabet.map( text => (
@@ -113,9 +146,9 @@ sketch.draw( ( time, center, favoriteColor ) => {
         duration: 1,
         lerpFn: mappers.lerpPoints,
         // easingFn: easing.easeInOutCubic,
-        // easingFn: easing.easeInOutQuad,
-        // currentTime: map(progression, 0, 1, 0, alphabet.length-1)+time,
-        currentTime: map(progression, 0, 1, 0, 2)//+time
+        // easingFn: easing.easeInOutSine,
+        easingFn: easing.easeInOutQuad,
+        currentTime: map(progression, 0, 1, 0, 4)//+time
       }) 
     },
     () => {
@@ -130,14 +163,13 @@ sketch.draw( ( time, center, favoriteColor ) => {
         easingFn: easing.easeInOutExpo,
         // easingFn: easing.easeInOutQuart,
         // easingFn: easing.easeInOutCirc,
-        // easingFn: easing.easeInOutCubic,
+        easingFn: easing.easeInOutCubic,
         currentTime: (
-          +time//+timeSpeed
+          +time
+          +gt
           +vectorsListProgression*2
-          // +vectorIndexProgression/10
         )
       })
-
 
       position.add( vector )
 
@@ -156,15 +188,14 @@ sketch.draw( ( time, center, favoriteColor ) => {
       vertex( position.x, position.y, position.z )
     },
     ( vectorIndexProgression, chunkIndex = 1 ) => {
-      stroke(colors.test({
+      stroke(colors.rainbow({
         hueOffset: (
           +time
-          //+timeSpeed
         ),
         // opacityFactor: 1.5,
-        hueIndex: mappers.fn(noise(chunkIndex, vectorIndexProgression*2), 0, 1, -PI/2, PI/2)*8,
-        opacityFactor: mappers.fn(noise(chunkIndex, vectorIndexProgression*2), 0, 1, 5, 1.5),
-        // opacityFactor: mappers.fn(sin(chunkIndex*10+time+vectorIndexProgression*50), -1, 1, 5, 1.5),
+        hueIndex: mappers.fn(noise(chunkIndex, vectorIndexProgression*3), 0, 1, -PI/2, PI/2)*16,
+        opacityFactor: mappers.fn(noise(chunkIndex, vectorIndexProgression*2), 0, 1, 4, 1.5),
+        // opacityFactor: mappers.fn(sin(chunkIndex*0+time+vectorIndexProgression*10), -1, 1, 5, 1.5),
       }))
 
       strokeWeight(5)
@@ -172,7 +203,7 @@ sketch.draw( ( time, center, favoriteColor ) => {
 
       // pop()
     },
-    false,
+    0,
     false,
     1
   )
@@ -181,10 +212,10 @@ sketch.draw( ( time, center, favoriteColor ) => {
 
   stroke(favoriteColor)
   fill(128, 128, 255, 32)
-  strokeWeight(4)
-  drawLine( end )
-  drawLine( start )
-  drawLine( middle )
+  strokeWeight(2)
+  drawLine( end, POINTS )
+  drawLine( start, POINTS )
+  drawLine( middle, POINTS )
 
   // translate(0, 0, 1)
   // strokeWeight(8)
