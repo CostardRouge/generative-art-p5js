79a5990efaa3197d6d679fc850a1bede504f7495
Steeve Pommier <CostardRouge@users.noreply.github.com>
2024-09-24 21:16:45 +0200

    export loop option

--- utils/recorder.js
+++ utils/recorder.js
@@ -9,27 +9,51 @@ const recorder = {
       format: options.get('recording-format'),
       quality: "best",
       framerate: options.get('recording-framerate'),
-      verbose: true,
+      verbose: false,
       name: sketch.name,
       workersPath: "libraries/",
     })
   },
-  start: () => {
+  start: (maximumFrames) => {
     if (true === recorder.recording) {
       return;
     }
 
-    recorder.createRecorder();
+    if (maximumFrames) {
+      const progressBar = document.getElementById('progressBar') ?? document.createElement('div');
+      progressBar.id = 'progressBar';
+
+      document.getElementsByTagName('main')[0].prepend(progressBar);
+  
+      const style = document.createElement('style');
+      style.innerHTML = `
+        #progressBar {
+          position: absolute;
+          top: 0;
+          left: 0;
+          height: 4px;
+          background-color: red;
+          width: 0;
+          z-index: 10;
+        }
+      `;
+      document.head.appendChild(style);
+    }
 
     recorder.recording = true;
-    document.body.classList.add("recording");
 
+    recorder.createRecorder();
     recorder.capturer.start();
 
     recorder.savedFramesCount = 0;
+    recorder.maximumFrames = maximumFrames;
+
+    document.body.classList.add("recording");
   },
   stop: () => {
     recorder.recording = false;
+    recorder.maximumFrames = undefined;
+
     document.body.classList.remove("recording");
 
     recorder.capturer.stop();
@@ -38,7 +62,17 @@ const recorder = {
   render: () => {
     requestAnimationFrame(recorder.render);
 
-    debug.createElement( "body", "recorder-saved-frames", () => recorder.savedFramesCount, !recorder.recording)
+    debug.createElement( "body", "recorder-saved-frames", () => {
+      if (recorder.maximumFrames) {
+        return `${recorder.savedFramesCount} / ${recorder.maximumFrames}`
+      }
+
+      return recorder.savedFramesCount
+    }, !recorder.recording)
+
+    if (recorder.maximumFrames && document.getElementById('progressBar')) {
+      document.getElementById('progressBar').style.width = (recorder.savedFramesCount / recorder.maximumFrames) * 100 + '%';
+    }
 
     if (undefined === recorder.capturer) {
       return;
@@ -51,6 +85,12 @@ const recorder = {
     const canvasElement = sketch?.engine?.getCanvasElement();
 
     if (undefined === canvasElement) {
+      return;
+    }
+
+    if (recorder.maximumFrames === recorder.savedFramesCount) {
+      recorder.stop();
+      requestAnimationFrame(recorder.render);
       return;
     }
 
