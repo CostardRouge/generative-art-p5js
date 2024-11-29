7aaec14ce39e8546ea1e8f3cf60211153098fb8b
Steeve Pommier <CostardRouge@users.noreply.github.com>
2024-11-30 00:55:28 +0100

    censored v7

--- sketches/censored/censored-v7-pixalated-gradient/index.js
+++ sketches/censored/censored-v7-pixalated-gradient/index.js
@@ -9,12 +9,12 @@ const canvases = {}
 sketch.setup(() => {
   canvases.buffer = createGraphics(
     sketch?.engine?.canvas?.width,
-    sketch?.engine?.canvas?.height
+    sketch?.engine?.canvas?.height/2
   );
 
   canvases.pixilated = createGraphics(
     sketch?.engine?.canvas?.width,
-    sketch?.engine?.canvas?.height
+    sketch?.engine?.canvas?.height/2
   );
 
   canvases.pixilated.pixelDensity(0.1)
@@ -137,117 +137,123 @@ class Spiral {
   }
 }
 
-
 function drawShape( {
   canvas, start, end, precision = 1 / 10, size
 }) {
+  canvases.buffer.background(0)
 
-  canvases.buffer.noStroke()
-  canvases.buffer.background(200)
+  const branchCount = 12;
 
   for (let stepIndex = 0; stepIndex < 1; stepIndex += precision) {
     const position = p5.Vector.lerp(start, end, stepIndex);
 
-    // canvas.point(position.x, position.y);
-
-    const opacityFactor = map(
-      stepIndex,
-      0,
-      1,
-      map(
-        Math.sin(stepIndex*30 + animation.sinAngle * 3),
-        -1,
-        1,
-        1,
-        1.3//*pow(lerpIndex, 1.1)
-      ),
-      1
-    );
-
-    const xOffset = size
-    const yOffset = mappers.fn(
-      Math.cos(animation.cosAngle*2+stepIndex*6), -1, 1, -size, size,
-      easing.easeInOutSine
-    );
-
-    const thickness = 50//mappers.fn(Math.sin(animation.sinAngle+stepIndex*3), -1, 1, 60, 120, easing.easeInOutElastic);
-    const branchCount = 9//mappers.fn(Math.cos(animation.sinAngle*6+stepIndex*9), -1, 1, 3, 9, easing.easeInOutSine);
 
     for (let branchIndex = 0; branchIndex <= branchCount; branchIndex++) {
       const branchProgression = branchIndex / branchCount;
+      const thickness = mappers.fn(Math.sin(animation.sinAngle*8+stepIndex*2+branchProgression*6), -1, 1, 60, 120, easing.easeInOutExpo);
+      // const s = mappers.fn(Math.sin(animation.sinAngle+stepIndex+branchIndex), -1, 1, -size/2, size/2, easing.easeInOutElastic);
 
-      const x1 = animation.ease({
-        values: [ position.x - xOffset, position.x + xOffset],
-        currentTime: animation.time,
-        easingFn: easing.easeInOutCubic
-      })
+      const x = position.x+mappers.fn(
+        noise(
+          stepIndex+animation.time/2,
+          branchIndex-animation.time
+        ),
+        0, 1,
+        -size, size,
+        easing.easeInOutQuad
+      )
 
-      const y1 = animation.ease({
-        values: [ position.y - yOffset, position.y + yOffset],
-        currentTime: animation.time,
-        easingFn: easing.easeInOutCubic
-      })
 
-      const x = lerp(
-        -x1-y1,
-        x1-y1,
-        branchProgression
+      
+      
+      // animation.ease({
+      //   values: [ -size/2+400, size*2-120 ],
+      //   currentTime: (
+      //     animation.time*3
+      //     // // +position.x*branchProgression
+      //     // +Math.cos(animation.sinAngle+branchProgression/2)
+      //     // +Math.sin(animation.sinAngle*2+branchProgression)
+      //     +branchProgression/2
+      //     +stepIndex//x*(Math.cos(animation.sinAngle+branchProgression/2))
+      //   ),
+      //   easingFn: easing.easeInOutCubic
+      // })
+      const y = position.y//+mappers.fn(noise(x/2000, animation.time), 0, 1, -10, 10, easing.easeInOutElastic);
+      
+      // position.y+mappers.fn(
+      //   Math.sin(animation.sinAngle+stepIndex+branchIndex), -1, 1, -size/2, size/2, easing.easeInOutElastic);
+      
+      // animation.ease({
+      //   values: [ -s+thickness, s-thickness ],
+      //   currentTime: (
+      //     animation.time*3
+      //     +branchIndex*2
+      //     +stepIndex
+      //   ),
+      //   easingFn: easing.easeInOutExpo
+      // })
+
+      const a = stepIndex*stepIndex*8+branchProgression*20;
+
+      const opacityFactor = map(
+        stepIndex,
+        0,
+        1,
+        map(
+          Math.sin(stepIndex*30 + animation.sinAngle*3 + branchProgression*2),
+          -1,
+          1,
+          1,
+          1.3
+        ),
+        1
       );
-      const y = lerp(
-        y1+x1,
-        -y1+x1,
-        branchProgression
-      );
 
-      const a = stepIndex*stepIndex*4+branchIndex*2;
-
-      canvas.fill(
+      canvas.stroke(
         map(Math.sin(stepIndex*a+animation.sinAngle), -1, 1, 0, 360) / opacityFactor,
         map(Math.cos(stepIndex*a-animation.cosAngle), -1, 1, 0, 255) / opacityFactor,
         map(Math.sin(stepIndex*a+animation.sinAngle), -1, 1, 255, 0) / opacityFactor
       );
 
-      canvas.circle(x, y, thickness);
+      canvas.strokeWeight(thickness);
+      canvas.point(x, y);
     }
   }
 }
 
-sketch.draw((time) => {
+sketch.draw((time, _, favoriteColor) => {
   noSmooth();
   background(100)
 
   drawShape( {
     canvas: canvases.buffer,
-    start: createVector(width /2, 0 ),
-    end: createVector(width /2, height),
-    precision: 1/80,
-    size: width/1.5
+    start: createVector(width /2, -120 ),
+    end: createVector(width /2, height/2+120),
+    precision: 1/800,
+    size: width/2
   })
-
-  image(canvases.buffer, 0, 0);
-
-
-  return;
-
-
-
-
-  shapes[0].draw(canvases.buffer, 1 / 800);
 
   canvases.pixilated.image(canvases.buffer.get(), 0, 0, width, height/2);
 
+  push()
+    scale(1, -1);
+    image(canvases.pixilated, 0, -height, width, height/2);
+  pop()
+
   image(canvases.buffer, 0, 0, width, height/2);
 
-  scale(1, -1);
-  image(canvases.pixilated, 0, -height, width, height/2);
+  strokeWeight(2)
+  stroke(favoriteColor)
+  line(0, height/2, width, height/2)
+
 
 
   // canvases.pixilated.pixelDensity(0.085)
-  canvases.pixilated.pixelDensity(mappers.circularIndex(time*2, [1, 1, 0.1, 0.01, 0.05, 0.025, 0.085]))
+  canvases.pixilated.pixelDensity(mappers.circularIndex(time, [1, 1, 0.1, 0.05, 0.025, 0.085]))
   // canvases.pixilated.pixelDensity(animation.ease({
   //   values: [ 0.1, 0.01, 0.05, 0.025, 0.085],
   //   currentTime: time/2,
-  //   easingFn: easing.easeInOutExpo
+  //   easingFn: easing.easeInOutExpo_
   // }))
 
 });
